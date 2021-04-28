829df49092eb838a5cf6f459710a7f290e0df625
anton-k <anton.kholomiov@gmail.com>
2021-04-28 12:40:54 +0300

    Lending create draft

--- mlabs/src/Mlabs/Lending.hs
+++ mlabs/src/Mlabs/Lending.hs
@@ -1,5 +1,60 @@
 module Mlabs.Lending where
 
-import qualified PlutusTx.Prelude     as Plutus
+-- import qualified PlutusTx.Prelude     as Plutus
+
+import           Data.Aeson
+import           Data.Text (Text)
+import           GHC.Generics (Generic)
+
+import           Ledger                           hiding (singleton)
+-- import           Ledger.Constraints               as Constraints
+-- import           Ledger.Constraints.OnChain       as Constraints
+-- import           Ledger.Constraints.TxConstraints as Constraints
+import           Plutus.Contract
+import qualified PlutusTx
+import qualified Ledger.Typed.Scripts             as Scripts
+
+
+import           Playground.Contract (ToSchema)
+
+newtype Pool = Pool
+  { poolCurrency :: CurrencySymbol
+  }
+  deriving (Show)
+PlutusTx.unstableMakeIsData ''Pool
+
+data Action
+  = Create Pool
+  | Close
+  deriving (Show)
+
+PlutusTx.unstableMakeIsData ''Action
+
+data LendingDatum = LendingDatum
+  { ldCurrency :: CurrencySymbol
+  }
+
+PlutusTx.unstableMakeIsData ''LendingDatum
+
+data CreateParams = CreateParams
+  { cpCurrency :: CurrencySymbol
+  }
+  deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
+
+create :: HasBlockchainActions s => CreateParams -> Contract w s Text ()
+create _ = do
+  return ()
+
+data Lending
+instance Scripts.ScriptType Lending where
+  type RedeemerType Lending = Action
+  type DatumType    Lending = LendingDatum
+
+type LendingSchema =
+     BlockchainActions
+         .\/ Endpoint "create" ()
+
+-- endpoints :: Contract w LendingSchema Void ()
+-- endpoints = forever endpoints
 
 
