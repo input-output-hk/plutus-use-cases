c54f36c45e74fa9b33f91230e76af4fc62066ae0
volodyad <volodymyr.derecha@eleks.com>
2021-05-17 12:34:09 +0300

    add contract for NFT

--- eleks/nft/pab/Main.hs
+++ eleks/nft/pab/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts   #-}
 {-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE RankNTypes         #-}
 {-# LANGUAGE TypeApplications   #-}
 {-# LANGUAGE TypeFamilies       #-}
@@ -11,13 +12,19 @@
 
 module Main(main) where
 
-import           Control.Monad                       (void)
+import           Control.Monad                       (void, forM)
 import           Control.Monad.Freer                 (Eff, Member, interpret, type (~>))
 import qualified Control.Concurrent.STM                         as STM
 import           Control.Monad.Freer.Error           (Error)
+import           Control.Monad.Freer.Extras.Log          (LogMsg)
 import           Control.Monad.IO.Class              (MonadIO (..))
+import qualified Data.Monoid                             as Monoid
+import qualified Data.Map.Strict                         as Map
+import           Data.Text                        (Text, pack)
+import qualified Data.Text               as T
+import qualified Data.ByteString.Char8        as C
 import           Data.Aeson                          (FromJSON (..), Result (..), ToJSON (..), genericToJSON, genericParseJSON
-                                                     , defaultOptions, Options(..), decode, parseJSON, fromJSON)
+                                                     , defaultOptions, Options(..), decode, encode, parseJSON, fromJSON)
 import           Data.Text.Prettyprint.Doc           (Pretty (..), viaShow)
 import           GHC.Generics                        (Generic)
 import           Plutus.Contract                     (BlockchainActions, ContractError)
@@ -28,8 +35,7 @@ import           Plutus.PAB.Simulator                (SimulatorEffectHandlers)
 import qualified Plutus.PAB.Simulator                as Simulator
 import           Plutus.PAB.Types                    (PABError (..))
 import qualified Plutus.PAB.Webserver.Server         as PAB.Server
-import           Contracts.Currency                  as Currency
-import           Contracts.Auction                   as Auction
+import           Contracts.NFT                       as NFTMarket
 import           Wallet.Emulator.Types               (Wallet (..))
 import qualified Data.ByteString.Char8               as B
 import qualified Ledger.Value            as Value
@@ -37,6 +43,7 @@ import           Ledger.Value                        (TokenName (..), Value)
 import           Wallet.API                               (ownPubKey)
 import           Ledger                                   (CurrencySymbol(..), pubKeyAddress)
 import qualified Ledger.Typed.Scripts         as Scripts
+import           Plutus.PAB.Monitoring.PABLogMsg         (PABMultiAgentMsg)
 
 extract :: Maybe a -> a
 extract (Just x) = x          -- Sure, this works, but...
@@ -44,85 +51,80 @@ extract Nothing  = undefined
 
 main :: IO ()
 main = void $ Simulator.runSimulationWith handlers $ do
-    Simulator.logString @(Builtin StarterContracts) "Starting plutus-starter PAB webserver on port 8080. Press enter to exit."
+    Simulator.logString @(Builtin NFTMarketContracts) "Starting nft market place PAB webserver on port 8080. Press enter to exit."
     shutdown <- PAB.Server.startServerDebug
 
-    --------------------------
     let w1 = Wallet 1
-    let w2 = Wallet 2
-    let tokenName = TokenName $ B.pack "TestCurrency"
-    let tokenName1 = TokenName $ B.pack "TestCurrency"
     w1Address <- pubKeyAddress <$> Simulator.handleAgentThread w1 ownPubKey
-    currencyInstance1 <- Simulator.activateContract w1 CurrencyContract
-    void $ Simulator.waitForEndpoint currencyInstance1 "createNativeToken"
-    void $ Simulator.callEndpointOnInstance currencyInstance1 "createNativeToken" SimpleMPS {Currency.tokenName = tokenName, amount = 1}
-    nftTokenSymbol       <- flip Simulator.waitForState currencyInstance1 $ \json -> case fromJSON json of
-                    Success (Just cur) -> Just $ Currency.currencySymbol cur
-                    _                                             -> Nothing
-    Simulator.logString @(Builtin StarterContracts) $ "Initialization finished. Minted NFT token: " ++ show nftTokenSymbol
-   
-    currencyInstance11 <- Simulator.activateContract w1 CurrencyContract
-    void $ Simulator.callEndpointOnInstance currencyInstance11 "createNativeToken" SimpleMPS {Currency.tokenName = tokenName1, amount = 1}
-    nftTokenSymbol1       <- flip Simulator.waitForState currencyInstance11 $ \json -> case fromJSON json of
-                    Success (Just cur) -> Just $ Currency.currencySymbol cur
-                    _                                             -> Nothing
-    Simulator.logString @(Builtin StarterContracts) $ "Initialization finished. Minted NFT token 2: " ++ show nftTokenSymbol1
-    
-    auctionInstance1 <- Simulator.activateContract w1 AuctionContract
-    auctionInstance2 <- Simulator.activateContract w2 AuctionContract
-    currentSlotStm <- Simulator.currentSlot 
-    currentSlot <- liftIO $ STM.atomically currentSlotStm
-    let deadlineSlot = currentSlot + 10
-    void $ Simulator.waitForEndpoint auctionInstance1 "start"
-    void $ Simulator.callEndpointOnInstance auctionInstance1 "start" StartParams { spDeadline = deadlineSlot, spMinBid = 100, spCurrency=nftTokenSymbol, spToken=tokenName}
-    Simulator.waitNSlots 10
 
-    void $ Simulator.waitForEndpoint auctionInstance2 "bid"
-    void $ Simulator.callEndpointOnInstance auctionInstance2 "bid" BidParams { bpCurrency = nftTokenSymbol, bpToken=tokenName, bpBid = 1000 }
-    Simulator.waitNSlots 10
+    nftMarketInstance1 <- Simulator.activateContract w1 NFTStartContract
+    market       <- flip Simulator.waitForState nftMarketInstance1 $ \json -> case (fromJSON json :: Result (Monoid.Last (Either Text NFTMarket.NFTMarket))) of
+                    Success (Monoid.Last (Just (Right market))) -> Just market
+                    _                                             -> Nothing
 
-    void $ Simulator.waitForEndpoint auctionInstance1 "close"
-    void $ Simulator.callEndpointOnInstance auctionInstance1 "close" CloseParams { cpCurrency = nftTokenSymbol, cpToken=tokenName }
+    Simulator.logString @(Builtin NFTMarketContracts) $ "NFT Marketplace instance created: " ++ show market
+
+
+    cids <- fmap Map.fromList $ forM wallets $ \w -> do
+        cid <- Simulator.activateContract w $ NFTUserContract market
+        Simulator.logString @(Builtin NFTMarketContracts) $ "NFT user contract started for " ++ show w
+        _ <- Simulator.callEndpointOnInstance cid "funds" ()
+        v <- flip Simulator.waitForState cid $ \json -> case (fromJSON json :: Result (Monoid.Last (Either Text NFTMarket.MarketContractState))) of
+                Success (Monoid.Last (Just (Right (NFTMarket.Funds v)))) -> Just v
+                _                                                      -> Nothing
+        Simulator.logString @(Builtin NFTMarketContracts) $ "initial funds in wallet " ++ show w ++ ": " ++ show v
+        return (w, cid)
+
+    let nftTokenParams = NFTMarket.CreateParams { cpTokenName = "TestToken", cpDescription = C.pack "TestDescrition"}
+    Simulator.logString @(Builtin NFTMarketContracts) $ "nft token create params: " ++ show (encode nftTokenParams)
+    void $ Simulator.callEndpointOnInstance (cids Map.! Wallet 2) "create" nftTokenParams
+    _ <- flip Simulator.waitForState (cids Map.! Wallet 2) $ \json -> case (fromJSON json :: Result (Monoid.Last (Either Text NFTMarket.MarketContractState))) of
+        Success (Monoid.Last (Just (Right NFTMarket.Created))) -> Just ()
+        _                                                      -> Nothing
+    Simulator.logString @(Builtin NFTMarketContracts) "NFT token created"
     Simulator.waitNSlots 1
     ------------
     void $ liftIO getLine
     
-    Simulator.logString @(Builtin StarterContracts) "Balances at the end of the simulation"
+    Simulator.logString @(Builtin NFTMarketContracts) "Balances at the end of the simulation"
     b <- Simulator.currentBalances
-    Simulator.logBalances @(Builtin StarterContracts) b
+    Simulator.logBalances @(Builtin NFTMarketContracts) b
 
     shutdown
 
-data StarterContracts =
-    CurrencyContract 
-    | AuctionContract 
+data NFTMarketContracts =
+      NFTStartContract 
+    | NFTUserContract NFTMarket.NFTMarket
     deriving (Eq, Ord, Show, Generic)
 
-instance ToJSON StarterContracts where
+instance ToJSON NFTMarketContracts where
   toJSON = genericToJSON defaultOptions {
              tagSingleConstructors = True }
-instance FromJSON StarterContracts where
+instance FromJSON NFTMarketContracts where
   parseJSON = genericParseJSON defaultOptions {
              tagSingleConstructors = True }
 
-instance Pretty StarterContracts where
+instance Pretty NFTMarketContracts where
     pretty = viaShow
 
-handleStarterContract ::
+handleNFTMarketContract ::
     ( Member (Error PABError) effs
+    , Member (LogMsg (PABMultiAgentMsg (Builtin NFTMarketContracts))) effs
     )
-    => ContractEffect (Builtin StarterContracts)
+    => ContractEffect (Builtin NFTMarketContracts)
     ~> Eff effs
-handleStarterContract = Builtin.handleBuiltin getSchema getContract where
+handleNFTMarketContract = Builtin.handleBuiltin getSchema getContract where
     getSchema = \case
-        CurrencyContract -> Builtin.endpointsToSchemas @(Currency.CurrencySchema .\\ BlockchainActions)
-        AuctionContract -> Builtin.endpointsToSchemas @(Auction.AuctionSchema .\\ BlockchainActions)
+        NFTStartContract -> Builtin.endpointsToSchemas @(NFTMarket.MarketOwnerSchema .\\ BlockchainActions)
+        NFTUserContract _  -> Builtin.endpointsToSchemas @(NFTMarket.MarketUserSchema .\\ BlockchainActions)
     getContract = \case
-        CurrencyContract -> SomeBuiltin Currency.forgeCurrency
-        AuctionContract -> SomeBuiltin Auction.endpoints
+        NFTStartContract -> SomeBuiltin NFTMarket.ownerEndpoint
+        NFTUserContract market -> SomeBuiltin $ NFTMarket.userEndpoints market
 
-handlers :: SimulatorEffectHandlers (Builtin StarterContracts)
+handlers :: SimulatorEffectHandlers (Builtin NFTMarketContracts)
 handlers =
-    Simulator.mkSimulatorHandlers @(Builtin StarterContracts) [CurrencyContract, AuctionContract]
-    $ interpret handleStarterContract
+    Simulator.mkSimulatorHandlers @(Builtin NFTMarketContracts)[] --[NFTStartContract, NFTUserContract]
+    $ interpret handleNFTMarketContract
 
+wallets :: [Wallet]
+wallets = [Wallet i | i <- [1 .. 4]]
---
+++ eleks/nft/src/Contracts/NFT.hs
@@ -0,0 +1,331 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+
+{-# LANGUAGE StandaloneDeriving #-}
+{-# options_ghc -fno-warn-orphans       #-}
+-- | Implements a custom currency with a monetary policy that allows
+--   the forging of a fixed amount of units.
+module Contracts.NFT
+    ( NFTMarket (..)
+    , CreateParams (..)
+    , MarketUserSchema, MarketContractState (..)
+    , MarketOwnerSchema
+    , start, create
+    , ownerEndpoint, userEndpoints
+    ) where
+
+import           Control.Monad                    hiding (fmap)
+import qualified Data.Map                         as Map
+import qualified Data.ByteString.Char8        as C
+import qualified Data.Text               as T
+import           Data.Monoid                      (Last (..))
+import           Data.Proxy                       (Proxy (..))
+import           Data.Text                        (Text, pack)
+import           Data.Void                        (Void)
+import           Ledger                           hiding (singleton)
+import           Ledger.Constraints               as Constraints
+import           Ledger.Constraints.OnChain       as Constraints
+import           Ledger.Constraints.TxConstraints as Constraints
+import qualified Ledger.Typed.Scripts             as Scripts
+import           Ledger.Value                     (AssetClass (..), assetClass, assetClassValue, assetClassValueOf,
+                                                    symbols, unCurrencySymbol, unTokenName)
+import           Playground.Contract
+import           Plutus.Contract                  hiding (when)
+import qualified Plutus.Contracts.Currency        as Currency
+import qualified PlutusTx
+import           PlutusTx.Prelude                 hiding (Semigroup (..), unless)
+import           PlutusTx.Sqrt
+import           Prelude                          (Semigroup (..))
+import qualified Prelude
+import           Text.Printf                      (printf)
+
+marketplaceTokenName :: TokenName
+marketplaceTokenName = "NFTMarketplace"
+metadataTokenName = "NFTMetadata"
+
+-- Note: An orphan instance here because of the alias above.
+deriving anyclass instance ToSchema AssetClass
+
+data NFTMetadata = NFTMetadata
+    { 
+      nftTokenName:: TokenName
+    , nftMetaTokenName:: TokenName
+    , nftMetaDescription:: ByteString
+    , nftTokenSymbol :: CurrencySymbol
+    , nftMetadataTokenSymbol :: CurrencySymbol
+    }
+    deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
+
+instance Eq NFTMetadata where
+    {-# INLINABLE (==) #-}
+    x == y = nftTokenSymbol x PlutusTx.Prelude.== nftTokenSymbol y
+
+PlutusTx.makeIsDataIndexed ''NFTMetadata [('NFTMetadata, 0)]
+PlutusTx.makeLift ''NFTMetadata
+
+newtype NFTMarket = NFTMarket
+    { marketId :: AssetClass
+    } deriving stock    (Show, Generic)
+      deriving anyclass (ToJSON, FromJSON, ToSchema)
+      deriving newtype  (Prelude.Eq, Prelude.Ord)
+
+
+PlutusTx.makeLift ''NFTMarket
+    
+data NFTMarketAction = Create NFTMetadata | Swap
+    deriving Show
+
+PlutusTx.makeIsDataIndexed ''NFTMarketAction [ ('Create , 0)
+                                           , ('Swap,   1)
+                                           ]
+PlutusTx.makeLift ''NFTMarketAction
+
+{-# INLINABLE findOwnInput' #-}
+findOwnInput' :: ScriptContext -> TxInInfo
+findOwnInput' ctx = fromMaybe (error ()) (findOwnInput ctx)
+
+{-# INLINABLE valueWithin #-}
+valueWithin :: TxInInfo -> Value
+valueWithin = txOutValue . txInInfoResolved
+
+{-# INLINABLE findMarketDatum #-}
+findMarketDatum :: TxInfo -> DatumHash -> NFTMetadata
+findMarketDatum info h = case findDatum h info of
+    Just (Datum d) -> case PlutusTx.fromData d of
+        Just (NFTMeta nft) -> nft
+        _                -> traceError "error decoding data"
+    _              -> traceError "market input datum not found"
+    
+data NFTMarketDatum =
+      Factory [NFTMetadata]
+    | NFTMeta NFTMetadata
+    deriving stock (Show)
+
+PlutusTx.unstableMakeIsData ''NFTMarketDatum
+PlutusTx.makeLift ''NFTMarketDatum
+
+data Market
+instance Scripts.ScriptType Market where
+    type instance RedeemerType Market = NFTMarketAction
+    type instance DatumType Market = NFTMarketDatum
+{-# INLINABLE validateCreate #-}
+validateCreate :: NFTMarket
+               -> [NFTMetadata]
+               -> NFTMetadata
+               -> ScriptContext
+               -> Bool
+validateCreate NFTMarket{..} nftMetas nftMeta@NFTMetadata{..} ctx =
+    traceIfFalse "Marketplace not present" (assetClassValueOf (valueWithin $ findOwnInput' ctx) marketId == 1) &&
+    all (/= nftMeta) nftMetas &&                                                                                 
+    Constraints.checkOwnOutputConstraint ctx (OutputConstraint (Factory $ nftMeta : nftMetas) $ assetClassValue marketId 1) &&
+    Constraints.checkOwnOutputConstraint ctx (OutputConstraint (NFTMeta nftMeta) $ assetClassValue (assetClass nftMetadataTokenSymbol nftMetaTokenName) 1)
+  where
+    marketOutput :: TxOut
+    marketOutput = case [o | o <- getContinuingOutputs ctx, assetClassValueOf (txOutValue o) marketId == 1] of
+        [o] -> o
+        _   -> traceError "expected exactly one market output"
+
+mkNFTMarketValidator :: NFTMarket
+      -> NFTMarketDatum
+      -> NFTMarketAction
+      -> ScriptContext
+      -> Bool
+mkNFTMarketValidator market (Factory nfts) (Create nft) ctx = validateCreate market nfts nft ctx
+mkNFTMarketValidator _      _              _            _   = False
+ 
+
+marketInstance :: NFTMarket -> Scripts.ScriptInstance Market
+marketInstance market = Scripts.validator @Market
+    ($$(PlutusTx.compile [|| mkNFTMarketValidator ||])
+        `PlutusTx.applyCode` PlutusTx.liftCode market)
+     $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @NFTMarketDatum @NFTMarketAction
+
+marketScript :: NFTMarket -> Validator
+marketScript = Scripts.validatorScript . marketInstance
+
+marketAddress :: NFTMarket -> Ledger.Address
+marketAddress = Ledger.scriptAddress . marketScript
+
+-- | Parameters for the @create@-endpoint, which creates a new NFT.
+data CreateParams = CreateParams
+    { cpTokenName   :: TokenName    -- ^ NFT name
+    , cpDescription   :: !ByteString    -- ^ metadata
+    } deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
+
+-- | Creates a Marketplace "factory". This factory will keep track of the existing nft tokens
+start :: HasBlockchainActions s => Contract w s Text NFTMarket
+start = do
+    pkh <- pubKeyHash <$> ownPubKey
+    cs  <- fmap Currency.currencySymbol $
+           mapError (pack . show @Currency.CurrencyError) $
+           Currency.forgeContract pkh [(marketplaceTokenName, 1)]
+    let c    = assetClass cs marketplaceTokenName
+        market   = marketplace cs
+        inst = marketInstance market
+        tx   = mustPayToTheScript (Factory []) $ assetClassValue c 1
+    ledgerTx <- submitTxConstraints inst tx
+    void $ awaitTxConfirmed $ txId ledgerTx
+
+    logInfo @String $ printf "started Market %s at address %s" (show market) (show $ marketAddress market)
+    return market
+
+-- | Creates a liquidity pool for a pair of coins. The creator provides liquidity for both coins and gets liquidity tokens in return.
+create :: HasBlockchainActions s => NFTMarket -> CreateParams -> Contract w s Text ()
+create market CreateParams{..} = do
+    (oref, o, nftMetas) <- findNFTMarketFactory market
+
+    ownPK <- pubKeyHash <$> ownPubKey
+    nftTokenSymbol  <- fmap Currency.currencySymbol $
+        mapError (pack . show @Currency.CurrencyError) $
+        Currency.forgeContract ownPK [(cpTokenName, 1)]
+    let nftTokenAssetClass = assetClass nftTokenSymbol cpTokenName
+        nftValue = assetClassValue nftTokenAssetClass 1
+ 
+    let metadataTokenName = TokenName $ C.pack $ (read $ show cpTokenName) ++ "Metadata"
+    nftTokenMetadataSymbol <- fmap Currency.currencySymbol $
+        mapError (pack . show @Currency.CurrencyError) $
+        Currency.forgeContract ownPK [( metadataTokenName, 1)]
+    let nftTokenMetadataAssetClass = assetClass nftTokenMetadataSymbol metadataTokenName
+        nftMetadataVal    = assetClassValue nftTokenMetadataAssetClass 1
+        nftMetadata       = NFTMetadata {
+            nftTokenName = cpTokenName, 
+            nftMetaTokenName = metadataTokenName,
+            nftMetaDescription = cpDescription, 
+            nftTokenSymbol = nftTokenSymbol, 
+            nftMetadataTokenSymbol = nftTokenMetadataSymbol }
+
+    let marketInst = marketInstance market
+        mrScript = marketScript market
+        marketFactoryData   = Factory $ nftMetadata : nftMetas
+        nftMetadataData   = NFTMeta nftMetadata
+        marketVal    = assetClassValue (marketId market) 1
+   
+        lookups  = Constraints.scriptInstanceLookups marketInst        <>
+                   Constraints.otherScript mrScript                <>
+                   Constraints.unspentOutputs (Map.singleton oref o)
+
+        tx       = Constraints.mustPayToTheScript marketFactoryData marketVal                                  <>
+                   Constraints.mustPayToTheScript nftMetadataData nftMetadataVal                                   <>
+                   -- Constraints.mustPayToTheScript () nftValue                                                  <>
+                   Constraints.mustSpendScriptOutput oref (Redeemer $ PlutusTx.toData $ Create nftMetadata)    <>
+                   Constraints.mustPayToPubKey ownPK nftValue
+
+    ledgerTx <- submitTxConstraintsWith lookups tx
+    void $ awaitTxConfirmed $ txId ledgerTx
+
+    logInfo $ "created NFT: " ++ show nftMetadata
+
+    
+marketplace :: CurrencySymbol -> NFTMarket
+marketplace cs = NFTMarket $ assetClass cs marketplaceTokenName
+
+getNFTMarketDatum :: TxOutTx -> Contract w s Text NFTMarketDatum
+getNFTMarketDatum o = case txOutDatumHash $ txOutTxOut o of
+        Nothing -> throwError "datumHash not found"
+        Just h -> case Map.lookup h $ txData $ txOutTxTx o of
+            Nothing -> throwError "datum not found"
+            Just (Datum e) -> case PlutusTx.fromData e of
+                Nothing -> throwError "datum has wrong type"
+                Just d  -> return d
+
+findNFTMartketInstance :: HasBlockchainActions s => NFTMarket -> AssetClass -> (NFTMarketDatum -> Maybe a) -> Contract w s Text (TxOutRef, TxOutTx, a)
+findNFTMartketInstance market asset f = do
+    let addr = marketAddress market
+    logInfo @String $ printf "looking for NFTMarket instance at address %s containing asset %s " (show addr) (show asset)
+    utxos <- utxoAt addr
+    go  [x | x@(_, o) <- Map.toList utxos, assetClassValueOf (txOutValue $ txOutTxOut o) asset == 1]
+  where
+    go [] = throwError "NFTMarket instance not found"
+    go ((oref, o) : xs) = do
+        d <- getNFTMarketDatum o
+        case f d of
+            Nothing -> go xs
+            Just a  -> do
+                logInfo @String $ printf "found NFTMarket instance with datum: %s" (show d)
+                return (oref, o, a)
+
+
+findNFTMarketFactory :: HasBlockchainActions s => NFTMarket -> Contract w s Text (TxOutRef, TxOutTx, [NFTMetadata])
+findNFTMarketFactory nftm@NFTMarket{..} = findNFTMartketInstance nftm marketId $ \case
+    Factory nfts -> Just nfts
+    NFTMeta _    -> Nothing
+
+--
+-- | Gets the caller's funds.
+funds :: HasBlockchainActions s => Contract w s Text Value
+funds = do
+    pkh <- pubKeyHash <$> ownPubKey
+    os  <- map snd . Map.toList <$> utxoAt (pubKeyHashAddress pkh)
+    return $ mconcat [txOutValue $ txOutTxOut o | o <- os]
+ 
+ownerEndpoint :: Contract (Last (Either Text NFTMarket)) BlockchainActions Void ()
+ownerEndpoint = do
+    e <- runError start
+    tell $ Last $ Just $ case e of
+        Left err -> Left err
+        Right market -> Right market
+
+type MarketOwnerSchema =
+    BlockchainActions
+        .\/ Endpoint "start" ()
+
+        -- | Schema for the endpoints for users of Uniswap.
+type MarketUserSchema =
+    BlockchainActions
+        .\/ Endpoint "create" CreateParams
+        .\/ Endpoint "funds"  ()
+        .\/ Endpoint "stop"   ()
+-- | Type of the Uniswap user contract state.
+
+
+data MarketContractState =
+      Created
+    | Funds Value
+    | Stopped
+    deriving (Show, Generic, FromJSON, ToJSON)
+-- | Provides the following endpoints for users of a NFT marketplace instance:
+--
+-- [@create@]: Creates an nft token. The creator provides liquidity for both coins and gets liquidity tokens in return.
+userEndpoints :: NFTMarket -> Contract (Last (Either Text MarketContractState)) MarketUserSchema Void ()
+userEndpoints market =
+    stop
+        `select`
+    ((f (Proxy @"create") (const Created) create                 `select`
+      f (Proxy @"funds")  Funds           (\market' () -> funds))    >> userEndpoints market)
+  where
+    f :: forall l a p.
+         HasEndpoint l p MarketUserSchema
+      => Proxy l
+      -> (a -> MarketContractState)
+      -> (NFTMarket -> p -> Contract (Last (Either Text MarketContractState)) MarketUserSchema Text a)
+      -> Contract (Last (Either Text MarketContractState)) MarketUserSchema Void ()
+    f _ g c = do
+        e <- runError $ do
+            p <- endpoint @l
+            c market p
+        tell $ Last $ Just $ case e of
+            Left err -> Left err
+            Right a  -> Right $ g a
+
+    stop :: Contract (Last (Either Text MarketContractState)) MarketUserSchema Void ()
+    stop = do
+        e <- runError $ endpoint @"stop"
+        tell $ Last $ Just $ case e of
+            Left err -> Left err
+            Right () -> Right Stopped
