7e09c391dcd36b26e6b55fcc7f4b1d8a3e1f28ad
Mak Muftic <mak@nodefactory.io>
2021-06-01 12:47:00 +0200

    Add vault definition

---
+++ NodeFactory/stable-coin/src/NodeFactory/Plutus/Contracts/Vault.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module NodeFactory.Plutus.Contracts.Vault
+    () where
+
+import           Control.Monad             hiding (fmap)
+import           Data.Aeson                (FromJSON, ToJSON)
+import qualified Data.Map                  as Map
+import           Data.Monoid               (Last (..))
+import           Data.Text                 (Text, pack)
+import           GHC.Generics              (Generic)
+import           Plutus.Contract           as Contract hiding (when)
+import qualified PlutusTx
+import           PlutusTx.Prelude          hiding (Semigroup(..), unless)
+import           Ledger                    hiding (singleton)
+import           Ledger.Constraints        as Constraints
+import qualified Ledger.Typed.Scripts      as Scripts
+import           Ledger.Value              as Value
+import           Ledger.Ada                as Ada
+import           Plutus.Contracts.Currency as Currency
+import           Prelude                   (Semigroup (..))
+import qualified Prelude                   as Prelude
+
+-- contract param structure
+data Vault = Vault
+    { owner  :: !PubKeyHash      -- owner of the of the vault
+    , amount :: !Integer         -- amount of ADA in vault
+    } deriving (Show, Generic, FromJSON, ToJSON, Prelude.Eq, Prelude.Ord)
+
+PlutusTx.makeLift ''Vault
+
+data VaultRedeemer = Create | Close  -- TODO - add liquidation
+    deriving Show
+
+PlutusTx.unstableMakeIsData ''VaultRedeemer
+
+-- 
+
+data Vaulting
+instance Scripts.ScriptType Vaulting where
+    type instance DatumType Vaulting = Integer
+    type instance RedeemerType Vaulting = VaultRedeemer
+
+vaultInst :: Vault -> Scripts.ScriptInstance Vaulting
+vaultInst oracle = Scripts.validator @Oracling
+    ($$(PlutusTx.compile [|| mkOracleValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode oracle)
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @Integer @VaultRedeemer
+
+vaultValidator :: Vault -> Validator
+vaultValidator = Scripts.validatorScript . vaultInst
+
+vaultAddress :: Vault -> Ledger.Address
+vaultAddress = scriptAddress . vaultValidator
