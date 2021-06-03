1bc69ff49b2756671d6b9a58937670515bf288b8
Igor <svilenkov@users.noreply.github.com>
2021-06-04 00:25:23 +0200

    update plutus git commit hash and code for uniswap

--- AltLabs/dex-token-swap/pab/Main.hs
+++ AltLabs/dex-token-swap/pab/Main.hs
@@ -9,49 +9,40 @@
 {-# LANGUAGE TypeApplications   #-}
 {-# LANGUAGE TypeFamilies       #-}
 {-# LANGUAGE TypeOperators      #-}
-
 module Main
     ( main
     ) where
 
-import           Control.Monad                            (forM, forM_, void, when)
-import           Control.Monad.Freer                      (Eff, Member, reinterpret, type (~>))
-import           Control.Monad.Freer.Error                (Error)
-import           Control.Monad.Freer.Extras.Log           (LogMsg)
-import           Control.Monad.IO.Class                   (MonadIO (..))
-import           Data.Aeson                               (FromJSON, Result (..), ToJSON, encode, fromJSON)
-import           Data.Bifunctor                           (Bifunctor (first))
-import qualified Data.Map.Strict                          as Map
-import qualified Data.Monoid                              as Monoid
-import           Data.Row
-import qualified Data.Semigroup                           as Semigroup
-import           Data.Text                                (Text)
-import           Data.Text.Extras                         (tshow)
-import           Data.Text.Prettyprint.Doc                (Pretty (..), viaShow)
-import           GHC.Generics                             (Generic)
-import           Ledger
-import           Ledger.Ada
-import           Ledger.Constraints
-import           Ledger.Value                             as Value
-import           Playground.Schema                        (endpointsToSchemas)
-import           Plutus.Contract                          hiding (when)
-import qualified Plutus.Contracts.Currency                as Currency
-import qualified Plutus.Contracts.Uniswap                 as Uniswap
-import           Plutus.PAB.Effects.Contract              (ContractEffect (..), PABContract (..))
-import           Plutus.PAB.Effects.Contract.ContractTest (doContractInit, doContractUpdate)
-import           Plutus.PAB.Events.Contract               (ContractPABRequest)
-import           Plutus.PAB.Events.ContractInstanceState  (PartiallyDecodedResponse)
-import           Plutus.PAB.Monitoring.PABLogMsg          (ContractEffectMsg (..))
-import           Plutus.PAB.Simulator                     (SimulatorContractHandler, SimulatorEffectHandlers)
-import qualified Plutus.PAB.Simulator                     as Simulator
-import           Plutus.PAB.Types                         (PABError (..))
-import qualified Plutus.PAB.Webserver.Server              as PAB.Server
-import           Prelude                                  hiding (init)
-import           Wallet.Emulator.Types                    (Wallet (..), walletPubKey)
+import           Control.Monad                           (forM, void)
+import           Control.Monad.Freer                     (Eff, Member, interpret, type (~>))
+import           Control.Monad.Freer.Error               (Error)
+import           Control.Monad.IO.Class                  (MonadIO (..))
+import           Data.Aeson                              (FromJSON, Result (..), ToJSON, encode, fromJSON)
+import qualified Data.Map.Strict                         as Map
+import qualified Data.Monoid                             as Monoid
+import qualified Data.Semigroup                          as Semigroup
+import           Data.Text                               (Text)
+import           Data.Text.Prettyprint.Doc               (Pretty (..), viaShow)
+import           GHC.Generics                            (Generic)
+import           Ledger.Ada                              (adaSymbol, adaToken)
+import           Plutus.Contract
+import qualified Plutus.Contracts.Currency               as Currency
+import qualified Plutus.Contracts.Uniswap                as Uniswap
+import           Plutus.PAB.Effects.Contract             (ContractEffect (..))
+import           Plutus.PAB.Effects.Contract.Builtin     (Builtin, SomeBuiltin (..), type (.\\))
+import qualified Plutus.PAB.Effects.Contract.Builtin     as Builtin
+import qualified Plutus.PAB.Effects.ContractTest.Uniswap as US
+import           Plutus.PAB.Simulator                    (SimulatorEffectHandlers)
+import qualified Plutus.PAB.Simulator                    as Simulator
+import           Plutus.PAB.Types                        (PABError (..))
+import qualified Plutus.PAB.Webserver.Server             as PAB.Server
+import           Prelude                                 hiding (init)
+import           Wallet.Emulator.Types                   (Wallet (..))
+
 
 main :: IO ()
 main = void $ Simulator.runSimulationWith handlers $ do
-    Simulator.logString @Uniswap "Starting Uniswap PAB webserver on port 8080. Press enter to exit."
+    Simulator.logString @(Builtin UniswapContracts) "Starting Uniswap PAB webserver on port 8080. Press enter to exit."
     shutdown <- PAB.Server.startServerDebug
 
     cidInit  <- Simulator.activateContract (Wallet 1) Init
@@ -59,38 +50,38 @@ main = void $ Simulator.runSimulationWith handlers $ do
                     Success (Just (Semigroup.Last cur)) -> Just $ Currency.currencySymbol cur
                     _                                   -> Nothing
     _        <- Simulator.waitUntilFinished cidInit
-    Simulator.logString @Uniswap $ "Initialization finished. Minted: " ++ show cs
-    let coins = Map.fromList [(tn, Uniswap.Coin cs tn) | tn <- tokenNames]
+
+    Simulator.logString @(Builtin UniswapContracts) $ "Initialization finished. Minted: " ++ show cs
+
+    let coins = Map.fromList [(tn, Uniswap.Coin cs tn) | tn <- US.tokenNames]
         ada   = Uniswap.Coin adaSymbol adaToken
 
     cidStart <- Simulator.activateContract (Wallet 1) UniswapStart
     us       <- flip Simulator.waitForState cidStart $ \json -> case (fromJSON json :: Result (Monoid.Last (Either Text Uniswap.Uniswap))) of
                     Success (Monoid.Last (Just (Right us))) -> Just us
                     _                                       -> Nothing
-    Simulator.logString @Uniswap $ "Uniswap instance created: " ++ show us
+    Simulator.logString @(Builtin UniswapContracts) $ "Uniswap instance created: " ++ show us
 
-    cids <- fmap Map.fromList $ forM wallets $ \w -> do
+    cids <- fmap Map.fromList $ forM US.wallets $ \w -> do
         cid <- Simulator.activateContract w $ UniswapUser us
-        Simulator.logString @Uniswap $ "Uniswap user contract started for " ++ show w
+        Simulator.logString @(Builtin UniswapContracts) $ "Uniswap user contract started for " ++ show w
         _ <- Simulator.callEndpointOnInstance cid "funds" ()
         v <- flip Simulator.waitForState cid $ \json -> case (fromJSON json :: Result (Monoid.Last (Either Text Uniswap.UserContractState))) of
                 Success (Monoid.Last (Just (Right (Uniswap.Funds v)))) -> Just v
                 _                                                      -> Nothing
-        Simulator.logString @Uniswap $ "initial funds in wallet " ++ show w ++ ": " ++ show v
+        Simulator.logString @(Builtin UniswapContracts) $ "initial funds in wallet " ++ show w ++ ": " ++ show v
         return (w, cid)
 
     let cp = Uniswap.CreateParams ada (coins Map.! "A") 100000 500000
-    Simulator.logString @Uniswap $ "creating liquidity pool: " ++ show (encode cp)
+    Simulator.logString @(Builtin UniswapContracts) $ "creating liquidity pool: " ++ show (encode cp)
     _  <- Simulator.callEndpointOnInstance (cids Map.! Wallet 2) "create" cp
     flip Simulator.waitForState (cids Map.! Wallet 2) $ \json -> case (fromJSON json :: Result (Monoid.Last (Either Text Uniswap.UserContractState))) of
         Success (Monoid.Last (Just (Right Uniswap.Created))) -> Just ()
         _                                                    -> Nothing
-    Simulator.logString @Uniswap "liquidity pool created"
+    Simulator.logString @(Builtin UniswapContracts) "liquidity pool created"
 
     _ <- liftIO getLine
     shutdown
-
-data Uniswap
 
 data UniswapContracts =
       Init
@@ -99,65 +90,25 @@ data UniswapContracts =
     deriving (Eq, Ord, Show, Generic)
     deriving anyclass (FromJSON, ToJSON)
 
-instance PABContract Uniswap where
-    type ContractDef Uniswap = UniswapContracts
-    type State Uniswap = PartiallyDecodedResponse ContractPABRequest
-    serialisableState _ = id
-
 instance Pretty UniswapContracts where
     pretty = viaShow
 
 handleUniswapContract ::
     ( Member (Error PABError) effs
-    , Member (LogMsg ContractEffectMsg) effs
     )
-    => ContractEffect Uniswap
+    => ContractEffect (Builtin UniswapContracts)
     ~> Eff effs
-handleUniswapContract = \case
-    InitialState c -> case c of
-        Init           -> doContractInit init
-        UniswapStart   -> doContractInit start
-        UniswapUser us -> doContractInit $ userEndpoints us
-    UpdateContract c state p -> case c of
-        Init           -> doContractUpdate init               state p
-        UniswapStart   -> doContractUpdate start              state p
-        UniswapUser us -> doContractUpdate (userEndpoints us) state p
-    ExportSchema t -> case t of
-        Init          -> pure $ endpointsToSchemas @Empty
-        UniswapStart  -> pure $ endpointsToSchemas @Empty
-        UniswapUser _ -> pure $ endpointsToSchemas @Empty
-            -- TODO:
-            -- replace with (Marlowe.MarloweSchema .\\ BlockchainActions)
-            -- (needs some instances for the Marlowe types (MarloweParams, etc))
-    where
-        init          = first tshow initContract
-        start         = first tshow Uniswap.ownerEndpoint
-        userEndpoints = first tshow . Uniswap.userEndpoints
+handleUniswapContract = Builtin.handleBuiltin getSchema getContract where
+  getSchema = \case
+    UniswapUser _ -> Builtin.endpointsToSchemas @(Uniswap.UniswapUserSchema .\\ BlockchainActions)
+    UniswapStart  -> Builtin.endpointsToSchemas @(Uniswap.UniswapOwnerSchema .\\ BlockchainActions)
+    Init          -> Builtin.endpointsToSchemas @Empty
+  getContract = \case
+    UniswapUser us -> SomeBuiltin $ Uniswap.userEndpoints us
+    UniswapStart   -> SomeBuiltin Uniswap.ownerEndpoint
+    Init           -> SomeBuiltin US.initContract
 
-handlers :: SimulatorEffectHandlers Uniswap
-handlers = Simulator.mkSimulatorHandlers @Uniswap [] mlw where
-    mlw :: SimulatorContractHandler Uniswap
-    mlw =
-        Simulator.handleContractEffectMsg @Uniswap
-        . reinterpret handleUniswapContract
-
-initContract :: Contract (Maybe (Semigroup.Last Currency.Currency)) Currency.CurrencySchema Currency.CurrencyError ()
-initContract = do
-    ownPK <- pubKeyHash <$> ownPubKey
-    cur   <- Currency.forgeContract ownPK [(tn, fromIntegral (length wallets) * amount) | tn <- tokenNames]
-    let cs = Currency.currencySymbol cur
-        v  = mconcat [Value.singleton cs tn amount | tn <- tokenNames]
-    forM_ wallets $ \w -> do
-        let pkh = pubKeyHash $ walletPubKey w
-        when (pkh /= ownPK) $ do
-            tx <- submitTx $ mustPayToPubKey pkh v
-            awaitTxConfirmed $ txId tx
-    tell $ Just $ Semigroup.Last cur
-  where
-    amount = 1000000
-
-wallets :: [Wallet]
-wallets = [Wallet i | i <- [1 .. 4]]
-
-tokenNames :: [TokenName]
-tokenNames = ["A", "B", "C", "D"]
+handlers :: SimulatorEffectHandlers (Builtin UniswapContracts)
+handlers =
+    Simulator.mkSimulatorHandlers @(Builtin UniswapContracts) [] -- [Init, UniswapStart, UniswapUser ???]
+    $ interpret handleUniswapContract
