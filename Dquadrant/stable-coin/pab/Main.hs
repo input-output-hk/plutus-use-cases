55d62152affbc9ea20d441183960d86bf594428b
nabin <nabin@sireto.io>
2021-06-07 11:23:27 +0545

    Add activate wallets, start contract for W1 in PAB

--- Dquadrant/stable-coin/pab/Main.hs
+++ Dquadrant/stable-coin/pab/Main.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts   #-}
 {-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE RankNTypes         #-}
 {-# LANGUAGE TypeApplications   #-}
 {-# LANGUAGE TypeFamilies       #-}
@@ -17,7 +19,7 @@ import           Control.Monad.Freer.Error               (Error)
 import           Control.Monad.Freer.Extras.Log          (LogMsg)
 import           Control.Monad.IO.Class                  (MonadIO (..))
 import           Data.Aeson                          (FromJSON (..), ToJSON (..), genericToJSON, genericParseJSON
-                                                     , defaultOptions, Options(..))
+                                                     , defaultOptions, Options(..), Result(Success),fromJSON)
 import qualified Data.Map.Strict                         as Map
 import qualified Data.Monoid                             as Monoid
 import qualified Data.Semigroup                          as Semigroup
@@ -50,6 +52,19 @@ main =
     Simulator.logString @(Builtin TokenContracts) "Starting plutus-starter PAB webserver on port 8080. Press enter to exit."
     shutdown <- PAB.Server.startServerDebug
 
+
+    w1cid <- Simulator.activateContract (Wallet 1) TokenContract
+        
+    Simulator.logString @(Builtin TokenContracts) "Contract starting by wallet 1"
+
+    let i = 1 :: Integer
+    _ <- Simulator.callEndpointOnInstance w1cid "start" i
+
+
+    forM_ wallets $ \w -> do
+            cid <- Simulator.activateContract w  TokenContract
+            liftIO $ writeFile ('W' : show (getWallet w) ++ ".cid") $ show $ unContractInstanceId cid
+    
     void $ liftIO getLine
     
     Simulator.logString @(Builtin TokenContracts) "Balances at the end of the simulation"
@@ -58,6 +73,12 @@ main =
 
     shutdown
 
+
+waitForLast :: FromJSON a => ContractInstanceId -> Simulator.Simulation t a
+waitForLast cid =
+    flip Simulator.waitForState cid $ \json -> case fromJSON json of
+        Success (Monoid.Last (Just x)) -> Just x
+        _                       -> Nothing
 
 data TokenContracts = TokenContract
     deriving (Eq, Ord, Show, Generic)
@@ -69,9 +90,11 @@ instance FromJSON TokenContracts where
   parseJSON = genericParseJSON defaultOptions {
              tagSingleConstructors = True }
 
-
 instance Pretty TokenContracts where
     pretty = viaShow
+
+wallets :: [Wallet]
+wallets = [Wallet i | i <- [2 .. 4]]
 
 handleTokenContract ::
     ( Member (Error PABError) effs
