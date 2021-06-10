ace41396794147739e3721004f418666947ea3d6
volodyad <volodymyr.derecha@eleks.com>
2021-06-10 09:17:03 +0300

    update doc

--- eleks/nft/src/Contracts/NFT.hs
+++ eleks/nft/src/Contracts/NFT.hs
@@ -1,12 +1,8 @@
--- | A decentralized exchange for arbitrary token pairs following the
--- [Uniswap protocol](https://uniswap.org/whitepaper.pdf).
---
--- Details:
---
+
+-- Details:--
 --  - 'OffChain' contains the instance endpoints and client functionality
 --  - 'OnChain' contains the validation logic
 --  - 'Types' conains a few common datatypes for working with this contract
---  - 'Pool' contains functions needed by both on-chain and off-chain code
 --    related to working with liquidity pools.
 module Contracts.NFT
   (module OnChain
