05843035b9ea63befd168824c18c968cedde415d
nabin <nabin@sireto.io>
2021-06-17 13:56:10 +0545

    Add utxo based oracle to project

---
+++ Dquadrant/stable-coin/src/Plutus/Contracts/Oracle/PAB.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+
+module Plutus.Contracts.Oracle.PAB
+    ( OracleContracts (..)
+    ) where
+
+import           Data.Aeson                (FromJSON, ToJSON)
+import           Data.Text.Prettyprint.Doc (Pretty (..), viaShow)
+import           GHC.Generics              (Generic)
+import           Ledger
+
+import qualified Plutus.Contracts.Oracle.Core        as Oracle
+
+data OracleContracts = Init | Oracle CurrencySymbol | Swap Oracle.Oracle
+    deriving (Eq, Ord, Show, Generic, FromJSON, ToJSON)
+
+instance Pretty OracleContracts where
+    pretty = viaShow
