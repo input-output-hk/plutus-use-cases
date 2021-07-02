250a49aba2ccbd5301cd38049b3e9e8a1b9432d2
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2021-07-02 20:50:48 +0700

    add nft model description

---
+++ MetaLamp/nft-marketplace/src/Plutus/Contracts/NftMarketplace/OnChain/NFT.hs
@@ -0,0 +1,24 @@
+module Plutus.Contracts.NftMarketplace.OnChain.NFT where
+
+data NFT = NFT
+  { nftId :: Text
+  , nftName :: Text
+  , nftDescription :: Text
+  , nftIssuer :: Maybe PubKey
+  , nftIpfsCid :: Text
+  }
+
+-- Marketplace model:
+-- 1. Store only files from IPFS network
+-- 2. NFT ID is a hash of IpfsCid
+-- 3. Store "data NFT" above in scripts datum as (Map nftId "data NFT")
+-- 4. TokenName of a NFT is the nftId
+-- 5. Only user with NFT (inside his wallet) identified by the nftId has access to view "data NFT"
+-- 6. Nobody can modify "data NFT"
+-- 7. Nobody can mint another NFT with same nftId
+-- 8. Nobody can burn NFT (?)
+-- TODO do we store NFT metadata from Cardano in IPFS also?
+
+-- TODO wrap "data NFT" into (isOnSale, "data NFT")
+-- to give access to view "data NFT" which are on sale for other users
+-- (only user with NFT inside wallet could change isOnSale)
