1af131516802bc9fea0521b5999f118137133043
olgaklimenko <gumper190@gmail.com>
2021-07-13 14:45:38 +0700

    use commit sha in fetch-plutus-purs script

--- MetaLamp/lending-pool/client/scripts/fetch-plutus-purs.sh
+++ MetaLamp/lending-pool/client/scripts/fetch-plutus-purs.sh
@@ -6,4 +6,4 @@ git remote add origin -f https://github.com/input-output-hk/plutus
 git config core.sparseCheckout true
 echo 'web-common-plutus/*' >> .git/info/sparse-checkout
 echo 'web-common/*' >> .git/info/sparse-checkout
-git pull origin plutus-starter-devcontainer/v1.0.6
+git pull origin bd16cc29045ffc7eaa6beaabe3b985a56cb9292a  # plutus-starter-devcontainer/v1.0.6
