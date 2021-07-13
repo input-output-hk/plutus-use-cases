1baae03abe8cc9418cdaf9b10eef5afe70e4b8be
olgaklimenko <gumper190@gmail.com>
2021-07-13 12:53:06 +0700

    change readme

--- MetaLamp/lending-pool/client/scripts/fetch-plutus-purs.sh
+++ MetaLamp/lending-pool/client/scripts/fetch-plutus-purs.sh
@@ -6,4 +6,4 @@ git remote add origin -f https://github.com/input-output-hk/plutus
 git config core.sparseCheckout true
 echo 'web-common-plutus/*' >> .git/info/sparse-checkout
 echo 'web-common/*' >> .git/info/sparse-checkout
-git pull origin 5cdd2c3d708bf4c33514681dee096da6463273b7
+git pull origin plutus-starter-devcontainer/v1.0.6
