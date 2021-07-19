cc14e4fa1812270c309b6cac030de813f895d3ec
stanislav-az <st.zhdanovich@gmail.com>
2021-07-19 20:32:57 +0700

    add nix config

---
+++ MetaLamp/nft-marketplace/shell.nix
@@ -0,0 +1,21 @@
+let
+  packages = import ./.;
+  inherit (packages) pkgs plutus-starter;
+  inherit (plutus-starter) haskell;
+
+in
+  haskell.project.shellFor {
+    withHoogle = false;
+
+    nativeBuildInputs = with plutus-starter; [
+      hlint
+      cabal-install
+      nodejs
+      purs
+      spago
+      haskell-language-server
+      stylish-haskell
+      pkgs.niv
+      cardano-repo-tool
+    ];
+  }
