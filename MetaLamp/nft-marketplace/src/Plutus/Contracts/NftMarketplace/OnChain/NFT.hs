b9d704a8b8247e2d2d6192454e3466effa9ef797
stanislav-az <st.zhdanovich@gmail.com>
2021-07-20 15:09:04 +0700

    refactor marketplace model

--- MetaLamp/nft-marketplace/src/Plutus/Contracts/NftMarketplace/OnChain/NFT.hs
+++ MetaLamp/nft-marketplace/src/Plutus/Contracts/NftMarketplace/OnChain/NFT.hs
@@ -5,24 +5,26 @@ import           Ledger
 import           Plutus.Contract
 import           PlutusTx.Prelude hiding (Semigroup (..))
 
+-- newtype AssetClass = AssetClass { unAssetClass :: (CurrencySymbol, TokenName) }
 data NFT =
   NFT
-    { nftId          :: T.Text
+    { nftId          :: CurrencySymbol
     , nftName        :: T.Text
     , nftDescription :: T.Text
-    , nftIssuer      :: Maybe PubKeyHash
-    , nftIpfsCid     :: T.Text
+    , nftIssuer      :: Maybe PubKeyHash -- ???? what if issuer is the owner, wouldn't that violate privacy TODO rm field?
     }
 
 -- Marketplace model:
--- 1. Store only files from IPFS network
--- 2. NFT ID is a hash of IpfsCid
--- 3. Store "data NFT" above in scripts datum as (Map nftId "data NFT")
--- 4. TokenName of a NFT is the nftId
--- 5. Only user with NFT (inside his wallet) identified by the nftId has access to view "data NFT"
--- 6. Nobody can modify "data NFT"
--- 7. Nobody can mint another NFT with same nftId
--- 8. Nobody can burn NFT (?)
+-- . Store only files from IPFS network. Get the IpfsCid
+-- . NFT token should be minted first in a separate transaction so that state token won't be linked to IpfsCid
+-- . Minting policy is parametrized by IpfsCid
+-- . TokenName of a NFT is the IpfsCid
+-- . NFT ID is the CurrencySymbol
+-- . Store "data NFT" above in scripts datum as (Map nftId "data NFT")
+-- . Only user with NFT (inside his wallet) identified by the nftId has access to view "data NFT"
+-- . Nobody can modify "data NFT"
+-- . Nobody can mint another NFT with same nftId
+-- . Nobody can burn NFT (?)
 -- TODO do we store NFT metadata from Cardano in IPFS also?
 
 -- TODO wrap "data NFT" into (isOnSale, "data NFT")
