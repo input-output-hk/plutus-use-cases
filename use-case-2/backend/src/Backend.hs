f553250e0999be3a138ca692ca196eeeefc1448a
hsloan <ishaqsloan@gmail.com>
2021-07-27 22:16:21 +0000

    use websocket client for executeSwap on app backend

--- use-case-2/backend/src/Backend.hs
+++ use-case-2/backend/src/Backend.hs
@@ -7,7 +7,7 @@
 
 module Backend where
 
-import Control.Concurrent (threadDelay)
+import Control.Concurrent
 import Control.Exception
 import Control.Monad.Identity
 import Control.Monad.IO.Class
@@ -15,6 +15,8 @@ import Control.Monad.Logger
 import Control.Monad.Reader
 import Data.Aeson.Lens
 import qualified Data.Aeson as Aeson
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as BS
 import Data.Int (Int32)
 import Data.Maybe
 import Data.Pool
@@ -40,6 +42,7 @@ import Rhyolite.Backend.App
 import Rhyolite.Backend.DB
 import Rhyolite.Backend.DB.Serializable
 import Rhyolite.Backend.Listen
+import Rhyolite.Backend.WebSocket
 import Safe (lastMay)
 import Statistics.Regression
 
@@ -51,6 +54,7 @@ import Common.Schema
 import Common.Plutus.Contracts.Uniswap.Types
 
 import Network.HTTP.Client hiding (Proxy)
+import qualified Network.WebSockets as WS
 import Control.Lens
 
 backend :: Backend BackendRoute FrontendRoute
@@ -201,12 +205,38 @@ executeSwap httpManager pool contractId (coinA, amountA) (coinB, amountB) = do
   print ("executeSwap: sending request to pab..." :: String)
   startTime <- getCurrentTime
   _ <- httpLbs req httpManager
+  eitherObState <- newEmptyMVar
+  _ <- WS.runClient "127.0.0.1" 8080 ("/ws/" ++ contractId) $ \conn -> do
+    threadDelay 10000000
+    let processData = do
+          incomingData :: ByteString <- WS.receiveData conn
+          let val :: Either String Aeson.Value = Aeson.eitherDecode' $ BS.fromStrict incomingData
+          print ("executeSwap: the value of val is " ++ show val) -- DEBUG
+          case val of
+            Left err -> do
+              print ("executeSwap: Left error is  " ++ show err) -- DEBUG
+              processData -- putMVar eitherObState $ Left err
+            Right obj -> do
+              print ("executeSwap: obj is  " ++ show obj ++ " \n") -- DEBUG
+              let swapTag = obj ^. key "contents" . key "Right" . key "tag" . _String
+                  txFeeDetails = obj ^. key "contents" . key "Right"
+                    . key "contents" . nth 0 . key "txFee" . key "getValue" . nth 0 . nth 1 . nth 0 . _Array
+                  aesArr = obj ^. key "contents" . key "Right"
+                    . key "contents" . _Array
+                  scrSize = fromMaybe (Aeson.Number 0) $ lastMay $ V.toList aesArr
+              print ("executeSwap: swapTag is  " ++ show swapTag) -- DEBUG
+              if swapTag == "Swapped" then putMVar eitherObState $ Right $ (Aeson.Array txFeeDetails, scrSize) else processData
+    fid <- forkIO processData
+    print ("executeSwap: closing websocket connection..." :: String) -- DEBUG
+    _ <- killThread fid
+    WS.sendClose conn ("" :: Text)
   print ("executeSwap: request sent." :: String)
   -- Allow enough time to pass for observable state to be updated (10 secs)
-  threadDelay 10000000
   endTime <- getCurrentTime
-  eitherObState <- fetchObservableStateFees httpManager contractId
-  case eitherObState of
+  print ("executeSwap: endTime set..." :: String)
+  eitherObState' <- takeMVar eitherObState
+  print ("executeSwap: eitherObState'." ++ show eitherObState')
+  case eitherObState' of
     Left err -> return $ Left err
     Right (txFeeDetails, Aeson.Number scrSize) -> case txFeeDetails of
       Aeson.Array xs -> case V.toList xs of
