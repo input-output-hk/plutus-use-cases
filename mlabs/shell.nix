aa3ec4fa2b1b4cb3cce0802bd74bd6680b1128ab
Vlad <cstmlcodes@gmail.com>
2021-08-09 19:17:38 +0100

    bugfix: nix-shell built from plutus (#114)

--- mlabs/shell.nix
+++ mlabs/shell.nix
@@ -1,68 +1,26 @@
-with import ./nix { };
-(plutus.plutus.haskell.project.shellFor (pab.env_variables // {
+let
+  srcs = import ./nix/sources.nix;
+  pkgs = import srcs.nixpkgs { };
+  plutusRepo = "https://github.com/input-output-hk/plutus";
+  nivPins = builtins.fromJSON (builtins.readFile ./nix/sources.json);
+  plutus = builtins.fetchGit {
+    url = plutusRepo;
+    rev = nivPins.plutus.rev;
+  };
+  plutusShell = import "${plutus}/shell.nix" { };
+  newShell = plutusShell.overrideAttrs (oldAttr:
+    oldAttr // rec {
 
-  # Select packages which should be added to the shell env
-  packages = ps:
-    [
-      # criterion 
-      # tasty-quickcheck
-    ];
+      # Add any extra packages you want in the environment here
+      extraInputs = with pkgs; [ nixfmt graphviz ];
 
-  # Select packages which should be added to the shell env, with their dependencies
-  # Should try and get the extra cardano dependencies in here...
-  additional = ps:
-    with ps; [
-      pab.plutus_ledger_with_docs
-      playground-common
-      plutus-contract
-      plutus-core
-      plutus-ledger-api
-      plutus-pab
-      plutus-tx
-      plutus-tx-plugin
-      plutus-use-cases
-      prettyprinter-configurable
-    ];
+      buildInputs = oldAttr.buildInputs ++ extraInputs;
 
-  withHoogle = true;
-
-  # Extra haskell tools (arg passed on to mkDerivation)
-  # Using the plutus.pkgs to use nixpkgs version from plutus (nixpkgs-unstable, mostly)
-  propagatedBuildInputs = with pkgs;
-    [
-      # Haskell Tools
-      cabal-install
-      entr
-      ghc
-      ghcid
-      git
-      haskellPackages.fourmolu
-      nixfmt
-      plutus.plutus.haskell-language-server
-      plutus.plutus.hlint
-      stack
-
-      # Makefile
-      gnumake
-      
-      # hls doesn't support preprocessors yet so this has to exist in PATH
-      haskellPackages.record-dot-preprocessor
-
-      # Graphviz Diagrams for documentation
-      graphviz
-
-      ### Pab
-      pab.plutus_pab_client
-
-      ### Example contracts
-      plutus.plutus-atomic-swap
-      plutus.plutus-currency
-
-    ] ++ (builtins.attrValues pab.plutus_pab_exes);
-
-  buildInputs = (with plutus.pkgs;
-    [ zlib pkg-config libsodium systemd ]
-    # Dependencies for MacOs
-    ++ (lib.optionals (!stdenv.isDarwin) [ R ]));
-  
-}))
+      shellHook = ''
+        echo "***-----------------***"
+        echo "*** MLabs Dev-Shell ***"
+        echo "***-----------------***"
+      '';
+    });
+in
+newShell
