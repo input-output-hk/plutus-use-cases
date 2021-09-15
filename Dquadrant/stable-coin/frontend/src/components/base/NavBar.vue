14735b2d8bf2ea320607498775228b83021dcfcd
nabin <nabin@sireto.io>
2021-09-15 10:00:17 +0545

    Update wallet select ui for oracle and non oracle page

--- Dquadrant/stable-coin/frontend/src/components/base/NavBar.vue
+++ Dquadrant/stable-coin/frontend/src/components/base/NavBar.vue
@@ -1,42 +1,56 @@
 <template>
   <b-navbar toggleable="lg" style="border-bottom: 1px solid #E8E8E8">
     <b-navbar-brand>
-      <img src="favicon.ico" alt="Logo" height="30px">
+      <img src="logo.png" alt="Logo" height="30px">
     </b-navbar-brand>
     <b-navbar-nav>
-      <b-nav-item :active="$router.currentRoute.path.startsWith('/market')" to="/market">Market</b-nav-item>
-      <b-nav-item :active="$router.currentRoute.path.startsWith('/mint')" to="/mint">Mint</b-nav-item>
-      <b-nav-item v-b-toggle.sidebar-1>Wallet</b-nav-item>
-      <b-sidebar id="sidebar-1" title="Wallet" shadow>
-        <SideBar/>
-      </b-sidebar>
+      <b-nav-item :active="$router.currentRoute.path.startsWith('/dashboard')" to="/dashboard">Dashboard</b-nav-item>
+      <b-nav-item :active="$router.currentRoute.path.startsWith('/oracle')" to="/oracle">Oracle</b-nav-item>
+      <b-nav-item :active="$router.currentRoute.path.startsWith('/configure')" to="/configure">Configure Contract</b-nav-item>
+      <b-nav-item :active="$router.currentRoute.path.startsWith('/mint')" to="/mint">Mint / Redeem</b-nav-item>
     </b-navbar-nav>
     <b-navbar-nav class="ml-auto">
       <slot></slot>
-      <b-form-select v-on:change="updateIndex" v-model="instanceIndex" :options="options"></b-form-select>
-      <span style="min-width: 250pt" class="p-2  text-muted text-sm-left">{{ instanceId }}</span>
-      <span style="min-width: 250pt" class="p-2  text-muted text-sm-left">{{ instanceDefinition }}</span>
+      <b-form-select v-if="!showOracleId" v-on:change="updateIndex" v-model="instanceIndex" :options="optionsWithNoOracle"></b-form-select>
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
-import SideBar from "./SideBar";
 
 export default {
   name: "NavBar",
-  components: {SideBar},
+  mounted() {
+    if(!this.showOracleId){
+      this.postStatusCall()
+    }
+  },
   computed: {
+    showOracleId(){
+      return this.$router.currentRoute.path.includes("oracle") ||
+             this.$router.currentRoute.path.includes("configure")
+    },
+    oracleId(){
+      const index =  this.$store.state.contract.instances.map((x, i) => {
+        return {text: 'Wallet-' + x.cicWallet.getWallet + " " + x.cicDefinition.tag, value: i}
+      })?.filter(item => item.text.includes("Oracle"))?.[0]["value"]
+      return this.$store.state.contract.instances[index]["cicContract"]["unContractInstanceId"]
+    },
     instanceId() {
       return this.$store.state.contract.instance.cicContract.unContractInstanceId
     },
     instanceDefinition() {
-      return this.$store.state.contract.instance.cicDefintion.tag
+      return this.$store.state.contract.instance.cicDefinition.tag
     },
-    options() {
-      return this.$store.state.contract.instances.map((x, i) => {
-        return {text: 'Wallet-' + x.cicWallet.getWallet + " " + x.cicDefintion.tag, value: i}
+    allOptions(){
+      return this.$store.state.contract.instances.map((x) => {
+        return {text: 'Wallet-' + x.cicWallet.getWallet + " " + x.cicDefinition.tag, value: x.cicContract.unContractInstanceId}
       })
+    },
+    optionsWithNoOracle() {
+      return this.$store.state.contract.instances.map((x, i) => {
+        return {text: 'Wallet-' + x.cicWallet.getWallet + " " + x.cicDefinition.tag, value: i}
+      }).filter(item => !item.text.includes("Oracle"))
     },
     instanceIndex: {
       get: function () {
@@ -49,7 +63,8 @@ export default {
   },
   watch: {
     instanceIndex(x) {
-      return this.$store.state.contract.instance = this.$store.state.contract.instances[x]
+      this.$store.state.contract.instance = this.$store.state.contract.instances[x]
+      this.postStatusCall()
     }
   },
   methods: {
@@ -57,13 +72,23 @@ export default {
       this.$store.state.contract.activeIndex=e
       this.$store.state.contract.instance=this.$store.state.contract.instances[e]
     },
+    postStatusCall(){
+      this.$http.post(
+          `instance/${this.$store.state.contract.instance.cicContract.unContractInstanceId}/endpoint/funds`,"\"\""
+      ).finally(() => this.$http.post(
+          `instance/${this.$store.state.contract.instance.cicContract.unContractInstanceId}/endpoint/currentRates`,"\"\""
+      )).finally(() => this.$http.post(
+          `instance/${this.$store.state.contract.instance.cicContract.unContractInstanceId}/endpoint/currentState`,"\"\""
+      ))
+    }
   },
   data: function () {
     return {}
   }
 }
 </script>
-
 <style scoped>
-
+.custom-select{
+  min-width: fit-content;
+}
 </style>
--- Dquadrant/stable-coin/frontend/src/components/base/SideBar.vue
+++ Dquadrant/stable-coin/frontend/src/components/base/SideBar.vue
@@ -1,86 +1,91 @@
 <template>
-  <div class="px-3 py-2">
+  <div class="mx-3 my-2">
     <div class="d-flex flex-column justify-content-lg-end border-bottom align-items-end pb-2">
-    <i style="color:gray">My balance</i>
-      <b class="balance-style">{{balance}} ada</b>
+      <i style="color:gray">My balance</i>
+      <b class="balance-style">{{ balance.ada }} Lovelace</b>
     </div>
-    <b-row v-for="(token,index) in tokens" :key="token.id" class="border-bottom pt-3 pb-3 d-flex justify-content-center align-items-center">
-      <b-col>
-        {{token.token.substring(0,5)}}......{{token.token.substring(token.token.length-5,)}}
-      </b-col>
-      <b-col>
-        <div v-if="token.isNFT" class="nft-tag-style">
-        NFT
-        </div>
-      </b-col>
-      <b-col>
-        <b-button v-b-modal="token.id" variant="success" @click="tokenClicked = token.id; selectedIndex = index">
-          Sell
-        </b-button>
-      </b-col>
-    </b-row>
-    <b-modal :id="tokenClicked" title="Sell Token" ref="my_modal" hide-footer>
+    <b-container>
+      <b-row v-for="(token,index) in balance.tokens" :key="index"
+             class="border-bottom mt-3 mb-3 d-flex justify-content-center align-items-center">
+        <b-col class="col-8 py-1" >
+          {{ token.currency.substring(0, 3) }}<span
+            class="text-muted">...</span>{{ token.currency.substring(token.currency.length - 3) }}<b class="text-lg-left
+"> : </b>{{ token.name }}
+        </b-col>
+        <b-col class="col-2 p-1">
+          <div v-if="token.value===1" class="nft-tag-style">
+            NFT
+          </div>
+        </b-col>
+        <b-col class="col-2 p-1">
+          <b-button v-b-modal="''+token.index" variant="success" @click="tokenClicked = token;">
+            Sell
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-modal size="lg" :id="''+tokenClicked.index" title="Sell Token" ref="my_modal" hide-footer>
       <b-form @submit.prevent="onSell()">
-        <b>Token: {{tokens[selectedIndex].token}}</b>
-        <b-form-group label="Enter amount" label-for="amount">
-          <b-form-input id="amount" v-model="amount" ref="input_number" type="number" placeholder="Enter your amount" required>
-          </b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="success">Sell</b-button>
+        <div class="py-1"><strong>Policy : </strong><span class="text-sm-left">{{ tokenClicked.currency }}</span></div>
+        <div class="py-1"><strong>Token:</strong>&nbsp; {{ tokenClicked.name }}</div>
+        <b-form-input class="my-3" id="amount" v-model="amount" ref="input_number" type="number" placeholder="Enter Sell price in Lovelace"
+                      required>
+        </b-form-input>
+        <b-button class="my-2" type="submit" variant="success">Sell</b-button>
       </b-form>
     </b-modal>
   </div>
 </template>
 
 <script>
-export default{
+export default {
   name: "SideBar",
-  methods:{
-    onSell() {
-      console.log("Amount response:", this.$refs.input_number.value)
-      this.$refs["my_modal"].hide();
+  computed: {
+    balance() {
+      return this.$store.state.contract.funds
     }
   },
-  data:() => {
-    return{
-      balance: 3000,
-      amount:"",
-      tokenClicked:"",
-      selectedIndex:0,
-      tokens:[
-        {
-          id:"1",
-          token: "23ir32in2o2okr2koiudqwubqdwoqwidoihhqwd",
-          isNFT: true,
-        },
-        {
-          id:"2",
-          token: "12der32in2o2okr2koiudqwubqdwoqwidoihhqwd",
-          isNFT: false,
-        },
-        {
-          id:"3",
-          token: "112332in2o2okr2koiudqwubqdwoqwidoihhqwd",
-          isNFT: true,
-        },
-        {
-          id:"4",
-          token: "231efw32in2o2okr2koidueqwrubqdwoqwidoihhqwd",
-          isNFT: false,
-        },
-      ],
+  methods: {
+    onSell() {
+      this.$task.do(
+          this.$http.post(
+              `/instance/${this.$store.state.contract.instance.cicContract.unContractInstanceId}/endpoint/sell`
+              , [{
+                "spItems":[{currency: this.tokenClicked.currency,token:this.tokenClicked.name,value:1}],
+                "spSaleType": "Primary",
+                "spCost":{currency: "",token:"",value:parseInt(this.$refs.input_number.value)}
+              }]
+          ).then(() => {
+                this.$task.infoMessage("Transaction Submitted. NFT placed in marketplace")
+                this.$bvModal.hide(''+this.tokenClicked)
+              }
+          )
+      )
+    },
+
+  },
+  data: () => {
+    return {
+      amount: "",
+      tokenClicked: {
+        currency: "",
+        name: "",
+        index: 0
+      },
+      selectedIndex: 0,
     }
   }
 }
 </script>
 
 <style scoped>
-.balance-style{
+.balance-style {
   display: flex;
   justify-content: flex-end;
   font-size: 24px;
 }
-.nft-tag-style{
+
+.nft-tag-style {
   font-size: 12px;
   color: #1EA2C4;
   display: flex;
