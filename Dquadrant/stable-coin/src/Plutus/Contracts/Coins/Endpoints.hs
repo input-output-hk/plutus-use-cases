81a1d9c3ae892f06c10e632426cdbb06cda8e484
nabin <nabin@sireto.io>
2021-09-15 10:11:01 +0545

    update to builtin data and contract using promises

--- Dquadrant/stable-coin/src/Plutus/Contracts/Coins/Endpoints.hs
+++ Dquadrant/stable-coin/src/Plutus/Contracts/Coins/Endpoints.hs
@@ -11,34 +11,12 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE NumericUnderscores #-}
-
--- {-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE DeriveAnyClass #-}
--- {-# LANGUAGE DeriveGeneric #-}
--- {-# LANGUAGE DerivingStrategies #-}
--- {-# LANGUAGE DerivingVia #-}
--- {-# LANGUAGE FlexibleContexts #-}
--- {-# LANGUAGE LambdaCase #-}
--- {-# LANGUAGE MonoLocalBinds #-}
--- {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
--- {-# LANGUAGE OverloadedStrings #-}
--- {-# LANGUAGE ScopedTypeVariables #-}
--- {-# LANGUAGE TemplateHaskell #-}
--- {-# LANGUAGE TypeApplications #-}
--- {-# LANGUAGE TypeFamilies     #-}
--- {-# LANGUAGE TypeOperators #-}
--- {-# LANGUAGE ViewPatterns #-}
--- {-# LANGUAGE NoImplicitPrelude #-}
--- {-# OPTIONS_GHC -Wno-name-shadowing #-}
--- {-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
--- {-# OPTIONS_GHC -fno-strictness #-}
--- {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:debug-context #-}
 
 module Plutus.Contracts.Coins.Endpoints
   ( 
     BankStateSchema,
-    coinsContract,
+    coinsEndpoints,
   )
 where
 
@@ -64,8 +42,6 @@ import Plutus.Contracts.Coins.Types
 import Plutus.Contracts.Oracle.Core
 import qualified Plutus.Contract.StateMachine     as SM
 import           Plutus.Contracts.Coins.CoinsStateMachine
-import qualified Plutus.Contracts.Utils.StateMachine as SmUtil
-
 import           Plutus.Contract.StateMachine     (SMContractError, StateMachineClient (..))
 import           Playground.TH                     (mkSchemaDefinitions)
 import           Ledger.Typed.Tx              (TypedScriptTxOut (..))
@@ -73,29 +49,30 @@ import qualified Data.Aeson.Types as Types
 import Data.Aeson (toJSON)
 import           Ledger.AddressMap                 (UtxoMap)
 
+-- Get forwarding monteragy policy script hash from state machine instance
+forwardMPS :: StateMachineClient CoinsMachineState BankInput -> MintingPolicyHash
+forwardMPS StateMachineClient {scInstance} = Scripts.forwardingMintingPolicyHash $ SM.typedValidator scInstance
 
-forwardMPS :: StateMachineClient CoinsMachineState BankInput -> MonetaryPolicyHash
-forwardMPS StateMachineClient {scInstance} = Scripts.forwardingMonetaryPolicyHash $ SM.typedValidator scInstance
-
+--Initial state of state machine to be set to all values to 0 with mps from script instance
 initialState :: StateMachineClient CoinsMachineState BankInput -> CoinsMachineState
 initialState smClient =
   CoinsMachineState
     { baseReserveAmount = 0,
       stableCoinAmount = 0,
       reserveCoinAmount = 0,
-      policyScript = forwardMPS smClient
+      policyScript = forwardMPS smClient,
+      bankFee = 1 % 1,
+      contractStatus = Running
     }
 
+--Helper to convert SM contract error to Text error
 mapError' :: Contract w s SMContractError a -> Contract w s Text a
 mapError' = mapError $ pack . Prelude.show
 
-start :: HasBlockchainActions s => BankParam -> Integer -> Contract w s Text ()
-start bankParam _ = do
-  let client = machineClient (scriptInstance bankParam) bankParam
-  void $ mapError' $ SM.runInitialise client (initialState client) mempty
-
---TODO make transistion to use whole oracle output instead of only values obtained from it
-smRunStep :: HasBlockchainActions s => BankParam -> BankInputAction -> Contract w s Text ()
+--TODO handle error
+--Run step function to construct lookups and execute run step of state machine
+-- Specific input to state machine i.e either minting or redeeming of coins is passed 
+smRunStep :: BankParam -> BankInputAction -> Contract w s Text ()
 smRunStep bankParam@BankParam{oracleParam} bankInputAction = do
   let client = machineClient (scriptInstance bankParam) bankParam
 
@@ -107,13 +84,10 @@ smRunStep bankParam@BankParam{oracleParam} bankInputAction = do
       let lookups = Constraints.unspentOutputs (Map.singleton oref o)
                     <> Constraints.otherScript (oracleValidator oracleParam)               
                                  
-          input =
-            BankInput
-              { bankInputAction = bankInputAction
-              , oracleOutput = (oref, txOutTxOut o, x)
-              }
+          input = BankInput bankInputAction (oref, txOutTxOut o, x)
+              
 
-      result <-  mapError' $ SmUtil.runStepWith client input lookups
+      result <-  mapError' $ SM.runStepWith lookups mempty client input
       
       case result of
         SM.TransitionFailure e -> do
@@ -122,155 +96,167 @@ smRunStep bankParam@BankParam{oracleParam} bankInputAction = do
         _ -> do
           void $ logInfo @Prelude.String $ "Endpoint call completed " ++ Prelude.show bankInputAction
 
+--TODO check for handling multiple start call
+--Endpoint to start the stable coint contract with  parameters supplied to banks
+start :: BankParam -> Promise w BankStateSchema Text ()
+start bankParam = (endpoint @"start") $ \_ -> do
+    let client = machineClient (scriptInstance bankParam) bankParam
+    void $ mapError' $ SM.runInitialise client (initialState client) mempty
+    void $ logInfo @Prelude.String $ "Stable coin contract started. " ++ Prelude.show (initialState client)
+
 --TODO check for validation in offchain
-mintStableCoin :: HasBlockchainActions s => BankParam -> EndpointInput -> Contract w s Text ()
-mintStableCoin bankParam@BankParam{oracleParam} EndpointInput{tokenAmount} = smRunStep bankParam $ MintStableCoin tokenAmount
+-- Contract endpoint for minting of stable coin
+mintStableCoin :: BankParam -> Promise w BankStateSchema Text ()
+mintStableCoin bankParam = 
+  (endpoint @"mintStableCoin") $ \EndpointInput{tokenAmount} -> do
+    smRunStep bankParam $ MintStableCoin tokenAmount
 
-redeemStableCoin :: HasBlockchainActions s => BankParam -> EndpointInput -> Contract w s Text ()
-redeemStableCoin bankParam EndpointInput{tokenAmount} = smRunStep bankParam $ RedeemStableCoin tokenAmount
+-- Contract endpoint for redeeming of stable coin to get ada back at current stable coin rate
+redeemStableCoin :: BankParam -> Promise w BankStateSchema Text ()
+redeemStableCoin bankParam = 
+  (endpoint @"redeemStableCoin") $ \EndpointInput{tokenAmount} -> do
+    smRunStep bankParam $ RedeemStableCoin tokenAmount
 
-mintReserveCoin :: HasBlockchainActions s => BankParam -> EndpointInput -> Contract w s Text ()
-mintReserveCoin bankParam EndpointInput{tokenAmount} = smRunStep bankParam $ MintReserveCoin tokenAmount
+-- Contract endpoint for minting of reserve coin
+mintReserveCoin :: BankParam -> Promise w BankStateSchema Text ()
+mintReserveCoin bankParam = 
+  (endpoint @"mintReserveCoin") $ \EndpointInput{tokenAmount} -> do
+    smRunStep bankParam $ MintReserveCoin tokenAmount
 
-redeemReserveCoin :: HasBlockchainActions s => BankParam -> EndpointInput -> Contract w s Text ()
-redeemReserveCoin bankParam EndpointInput{tokenAmount} = smRunStep bankParam $ RedeemReserveCoin tokenAmount
+-- Contract endpoint for redeeming of resever coin  to get ada back at current reserve rate
+redeemReserveCoin :: BankParam -> Promise w BankStateSchema Text ()
+redeemReserveCoin bankParam = 
+  (endpoint @"redeemReserveCoin") $ \EndpointInput{tokenAmount} -> do
+    smRunStep bankParam $ RedeemReserveCoin tokenAmount
 
+updateBankFee :: BankParam -> Promise w BankStateSchema Text ()
+updateBankFee bankParam = 
+  (endpoint @"updateBankFee") $ \BankFeeInput{percentNumerator,percentDenominator} -> do
+    let client = machineClient (scriptInstance bankParam) bankParam
+        input = UpdateBankFee percentNumerator percentDenominator
+        
+    result <- mapError' $ SM.runStep client input
+    case result of
+      SM.TransitionFailure e -> do
+        void $ logInfo @Prelude.String $ "Transistion Faliure "
+        throwError "Transistion Faliure"
+      _ -> do
+        void $ logInfo @Prelude.String $ "Endpoint call completed " ++ Prelude.show input
+
+updateContractStatus :: BankParam -> Promise w BankStateSchema Text ()
+updateContractStatus bankParam = 
+  (endpoint @"updateContractStatus") $ \ContractStatusInput{shouldPause} -> do
+    let client = machineClient (scriptInstance bankParam) bankParam
+        input = UpdateContractStatus shouldPause
+                
+    result <- mapError' $ SM.runStep client input
+
+    case result of
+      SM.TransitionFailure e -> do
+        void $ logInfo @Prelude.String $ "Transistion Faliure Couldn't proceed further."
+        throwError "Transistion Faliure"
+      _ -> do
+        void $ logInfo @Prelude.String $ "Endpoint call completed " ++ Prelude.show input
+
+
+--Endpoint definitions availabe for the stable coin contract
 type BankStateSchema =
-  BlockchainActions
-    .\/ Endpoint "start" Integer
+    Endpoint "start" Integer
     .\/ Endpoint "mintStableCoin" EndpointInput
     .\/ Endpoint "redeemStableCoin" EndpointInput
     .\/ Endpoint "mintReserveCoin" EndpointInput
     .\/ Endpoint "redeemReserveCoin" EndpointInput
+    .\/ Endpoint "updateBankFee" BankFeeInput
+    .\/ Endpoint "updateContractStatus" ContractStatusInput
 
     .\/ Endpoint "funds" Prelude.String
     .\/ Endpoint "currentState" Prelude.String
-    .\/ Endpoint "pegRate" Prelude.String
-    .\/ Endpoint "stableRate" Prelude.String
-    .\/ Endpoint "reserveRate" Prelude.String
     .\/ Endpoint "currentRates" Prelude.String
 
 --TODO writer value [Types.Value]
-coinsContract :: BankParam -> Contract [Types.Value] BankStateSchema Text ()
-coinsContract bankParam =
-  ( 
-    start'
-      `select`
-       mintStableCoin'
-      `select` redeemStableCoin'
-      `select` mintReserveCoin'
-      `select` redeemReserveCoin'
-
-      `select` ownFunds'
-      `select` currentState'
-      `select` pegToLovRate'
-      `select` stableToLovRate'
-      `select` reserveToLovRate'
-      `select` currentRates'
-  )
-    >> coinsContract bankParam
-  where
-    --TODO handle state for multiple start endpoint call
-    start' = endpoint @"start" >>= start bankParam
-    mintStableCoin' = endpoint @"mintStableCoin" >>= mintStableCoin bankParam
-    redeemStableCoin' = endpoint @"redeemStableCoin" >>= redeemStableCoin bankParam
-    mintReserveCoin' = endpoint @"mintReserveCoin" >>= mintReserveCoin bankParam
-    redeemReserveCoin' = endpoint @"redeemReserveCoin" >>= redeemReserveCoin bankParam
+--Starting point of the contract which combines all the endpoints to be available for call
+coinsEndpoints :: BankParam -> Contract [Types.Value] BankStateSchema Text ()
+coinsEndpoints bankParam = handleError handler (void selections) >> coinsEndpoints bankParam
+  where 
+    selections= selectList
+      [ 
+        start bankParam
+      , mintStableCoin bankParam
+      , redeemStableCoin bankParam
+      , mintReserveCoin bankParam
+      , redeemReserveCoin bankParam
+      , updateBankFee bankParam
+      , updateContractStatus bankParam
+      , ownFunds
+      , currentCoinsMachineState bankParam
+      , currentRates bankParam
+      ]
     
-    ownFunds' = endpoint @"funds" >> ownFunds bankParam
-    currentState' = endpoint @"currentState" >> currentCoinsMachineState bankParam
-    pegToLovRate' = endpoint @"pegRate" >> currentPegToLovelaceRate bankParam
-    stableToLovRate' = endpoint @"stableRate" >> currentStableToLovelaceRate bankParam
-    reserveToLovRate' = endpoint @"reserveRate" >> currentReserveToLovelaceRate bankParam
-    currentRates' = endpoint @"currentRates" >> currentRates bankParam
+    handler :: Prelude.Show a => a -> Contract w s e ()
+    handler e = do
+        Contract.logError $ Prelude.show e
 
-
-ownFunds:: HasBlockchainActions s => BankParam -> Contract [Types.Value ] s Text  ()
-ownFunds _ = do
+--Endpoint for getting current funds held in a users public key
+ownFunds:: Promise [Types.Value ] BankStateSchema Text  ()
+ownFunds = 
+  (endpoint @"funds") $ \_ -> do
     pk    <- ownPubKey
     utxos <- utxoAt $ pubKeyAddress pk
     let v = mconcat $ Map.elems $ txOutValue . txOutTxOut Prelude.<$> utxos
     logInfo @Prelude.String $ "own funds: " ++ Prelude.show (flattenValue v)
     tell [ toJSON v]
+                 
+--Endpoint for getting current state of state machine i.e current tokesn supply, base reserves etc.
+currentCoinsMachineState:: BankParam -> Promise [Types.Value ] BankStateSchema Text  ()
+currentCoinsMachineState bankParam = 
+  (endpoint @"currentState") $ \_ -> do
+    currentState <- mapError' $ getCurrentState bankParam
+    case currentState of
+      Just state -> do
+          logInfo @Prelude.String $ "Current state: " ++ Prelude.show state
+          let stateResponse = StateResponse{
+            currentCoinsState = state
+          }
+          tell [toJSON stateResponse]
+      Nothing -> logWarn @Prelude.String $ "Current state is not present yet."
 
-currentState :: ( SM.AsSMContractError e
-    , HasUtxoAt schema)
-    => BankParam -> Contract w schema e (Maybe (SM.OnChainState CoinsMachineState BankInput, UtxoMap))
-currentState bankParam = do
-  let client = machineClient (scriptInstance bankParam) bankParam
-  SM.getOnChainState client
-
-currentCoinsMachineState:: HasBlockchainActions s => BankParam -> Contract [Types.Value ] s Text  ()
-currentCoinsMachineState bankParam = do
-  currentStateVal <- mapError' $ currentState bankParam
-  case currentStateVal of
-    Just ((TypedScriptTxOut{tyTxOutData=state},_),_) -> do
-        logInfo @Prelude.String $ "Current state: " ++ Prelude.show state
-        let stateResponse = StateResponse{
-          currentCoinsState = state
-        }
-        tell [toJSON stateResponse]
-    Nothing -> logWarn @Prelude.String $ "Current state is not present yet."
-
-currentPegToLovelaceRate :: HasBlockchainActions s => BankParam -> Contract [Types.Value ] s Text  ()
-currentPegToLovelaceRate bankParam = do
-  oracle <- findOracle $ oracleParam bankParam
-  case oracle of
-    Nothing -> logWarn @Prelude.String "Oracle not found"
-    Just (oref, o, rate) -> do
-      logInfo @Prelude.String $ "Current state: " ++ Prelude.show rate
-      tell [toJSON rate]
-
---TODO Merge duplicated code on getting stable and reserve rate functions
-currentStableToLovelaceRate :: HasBlockchainActions s => BankParam -> Contract [Types.Value ] s Text  ()
-currentStableToLovelaceRate bankParam = do
-  oracle <- findOracle $ oracleParam bankParam
-  case oracle of
-    Nothing -> logWarn @Prelude.String "Oracle not found"
-    Just (oref, o, rate) -> do
-      currentStateVal <- mapError' $ currentState bankParam
-      case currentStateVal of
-          Just ((TypedScriptTxOut{tyTxOutData=state},_),_) -> do
-              logInfo @Prelude.String $ "Current state: " ++ Prelude.show state
-              let scRate = calcStableCoinRate state rate
-              tell [toJSON scRate]
-          Nothing -> logWarn @Prelude.String $ "Current state is not present yet."
-
-currentReserveToLovelaceRate :: HasBlockchainActions s => BankParam -> Contract [Types.Value ] s Text  ()
-currentReserveToLovelaceRate bankParam = do
-  oracle <- findOracle $ oracleParam bankParam
-  case oracle of
-    Nothing -> logWarn @Prelude.String "Oracle not found"
-    Just (oref, o, rate) -> do
-      currentStateVal <- mapError' $ currentState bankParam
-      case currentStateVal of
-          Just ((TypedScriptTxOut{tyTxOutData=state},_),_) -> do
-              logInfo @Prelude.String $ "Current state: " ++ Prelude.show state
-              let rcRate = calcReserveCoinRate bankParam state rate
-              tell [toJSON rcRate]
-          Nothing -> logWarn @Prelude.String $ "Current state is not present yet."
-
-currentRates :: HasBlockchainActions s => BankParam -> Contract [Types.Value ] s Text  ()
-currentRates bankParam = do
-  oracle <- findOracle $ oracleParam bankParam
-  case oracle of
-    Nothing -> logWarn @Prelude.String "Oracle not found"
-    Just (oref, o, rate) -> do
-      currentStateVal <- mapError' $ currentState bankParam
-      case currentStateVal of
-          Just ((TypedScriptTxOut{tyTxOutData=state},_),_) -> do
-              logInfo @Prelude.String $ "Current state: " ++ Prelude.show state
+--Endpoint for getting combined rates of peg, stable coin rate and reserve coin rate
+currentRates :: BankParam -> Promise [Types.Value ] BankStateSchema Text  ()
+currentRates bankParam = 
+  (endpoint @"currentRates") $ \_ -> do
+    oracle <- findOracle $ oracleParam bankParam
+    case oracle of
+      Nothing -> logWarn @Prelude.String "Oracle not found"
+      Just (oref, o, rate) -> do
+        currentState <- mapError' $ getCurrentState bankParam
+        case currentState of
+          Just state -> do
               let rcRate = calcReserveCoinRate bankParam state rate
                   scRate = calcStableCoinRate state rate
-
-                  rates = Rates 
-                                  {
-                                        pegRate = rate,
-                                        scRate = scRate,
-                                        rcRate = rcRate
-                                  }
+                  rates = Rates {
+                            pegRate = rate,
+                            scRate = scRate,
+                            rcRate = rcRate
+                  }
                   ratesResponse = RatesResponse{
                     currentCoinsRates = rates
                   }
+              logInfo @Prelude.String $ "Current rates: " ++ Prelude.show rates
               tell [toJSON ratesResponse]
           Nothing -> logWarn @Prelude.String $ "Current state is not present yet."
+
+--Helper fucntion for getting current state of state machine
+getCurrentState :: ( SM.AsSMContractError e)
+    => BankParam -> Contract w schema e (Maybe CoinsMachineState)
+getCurrentState bankParam = do
+  let client = machineClient (scriptInstance bankParam) bankParam
+  
+  currentStateVal <- SM.getOnChainState client
+  
+  case currentStateVal of
+      Just (onChainState, utxo) -> do
+        let SM.OnChainState{ocsTxOut=TypedScriptTxOut{tyTxOutData=state, tyTxOutTxOut}, ocsTxOutRef} = onChainState
+        pure $ Just state
+      Nothing -> do
+        logWarn @Prelude.String $ "Current state is not present yet."
+        pure $ Nothing
--- Dquadrant/stable-coin/src/Plutus/Contracts/Oracle/Core.hs
+++ Dquadrant/stable-coin/src/Plutus/Contracts/Oracle/Core.hs
@@ -25,8 +25,10 @@ module Plutus.Contracts.Oracle.Core
     , oracleAddress
     , OracleSchema
     , runOracle
+    , oracleContract
     , runMockOracle
     , findOracle
+    , mkOracleValidator
     ) where
 
 import           Control.Monad             hiding (fmap)
@@ -47,39 +49,44 @@ import           Plutus.Contracts.Currency as Currency
 import           Prelude                   (Semigroup (..))
 import qualified Prelude                   as Prelude
 
+--Paramenter data of oracle to be used in oraclle validator
 data Oracle = Oracle
-    { oNftSymbol   :: !CurrencySymbol
-    , oOperator    :: !PubKeyHash
-    , oFee         :: !Integer
-    } deriving (Prelude.Show, Generic, FromJSON, ToJSON, Prelude.Eq, Prelude.Ord)
+    { oNftSymbol   :: !CurrencySymbol -- Nft symbol to identify correct utxo for getting exchange rate
+    , oOperator    :: !PubKeyHash      -- Public key hash of operator of oracle like for updating the price
+    , oFee         :: !Integer          -- Fee taken by oracle to use its value as an exchange rate
+    } deriving (Prelude.Show, Generic, FromJSON, ToJSON)
 
 PlutusTx.makeLift ''Oracle
 PlutusTx.unstableMakeIsData ''Oracle
 
-
+--Redeemer for Update and Use action provided in oracle contract
 data OracleRedeemer = Update | Use
-    deriving Prelude.Show
+    -- deriving Prelude.Show
 
 PlutusTx.unstableMakeIsData ''OracleRedeemer
 
+--Construct a oracle token name as empty byte string
 {-# INLINABLE oracleTokenName #-}
 oracleTokenName :: TokenName
 oracleTokenName = TokenName emptyByteString
 
+--Construct a asset class of oracle symbol and oracle token name
 {-# INLINABLE oracleNftAsset #-}
 oracleNftAsset :: Oracle -> AssetClass
 oracleNftAsset oracle = AssetClass (oNftSymbol oracle, oracleTokenName)
 
+--Find value of oracle from tx output
 {-# INLINABLE oracleValue #-}
 oracleValue :: TxOut -> (DatumHash -> Maybe Datum) -> Maybe Integer
 oracleValue o f = do
     dh      <- txOutDatum o
     Datum d <- f dh
-    PlutusTx.fromData d
+    PlutusTx.fromBuiltinData d
 
+--Oracle validator for checking update and use of oracle value
 {-# INLINABLE mkOracleValidator #-}
 mkOracleValidator :: Oracle -> Integer -> OracleRedeemer -> ScriptContext -> Bool
-mkOracleValidator oracle x r ctx =
+mkOracleValidator oracle x r ctx = 
     traceIfFalse "token missing from input"  inputHasToken  &&
     traceIfFalse "token missing from output" outputHasToken &&
     case r of
@@ -139,11 +146,12 @@ oracleValidator = Scripts.validatorScript . oracleInst
 oracleAddress :: Oracle -> Ledger.Address
 oracleAddress = scriptAddress . oracleValidator
 
-startOracle :: forall w s. HasBlockchainActions s => Contract w s Text Oracle
+--Start oracle by first getting currecny symbol of nft to be used and make a oracle paramter using that currecny symbol
+startOracle :: forall w s. Contract w s Text Oracle
 startOracle = do
     logInfo @Prelude.String $ "Starting oracle "
     pkh <- pubKeyHash <$> Contract.ownPubKey
-    osc <- mapError (pack . Prelude.show) (forgeContract pkh [(oracleTokenName, 1)] :: Contract w s CurrencyError OneShotCurrency)
+    osc <- mapError (pack . Prelude.show) (mintContract pkh [(oracleTokenName, 1)] :: Contract w s CurrencyError OneShotCurrency)
     let cs     = Currency.currencySymbol osc
         oracle = Oracle
             { oNftSymbol   = cs
@@ -153,7 +161,10 @@ startOracle = do
     logInfo @Prelude.String $ "started oracle " ++ Prelude.show oracle
     return oracle
 
-updateOracle :: forall w s. HasBlockchainActions s => Oracle -> Integer -> Contract w s Text ()
+--Endpoint to update oracle which is only allowed by owner of the oracle provider
+-- Find the oracle valie if nothing found then submit oracle value to oracle script
+-- Otherwise spent previous ouput and construct new utxo with new oracle value at oracle address
+updateOracle :: forall w s. Oracle -> Integer -> Contract w s Text ()
 updateOracle oracle x = do
     m <- findOracle oracle
     let c = Constraints.mustPayToTheScript x $ assetClassValue (oracleNftAsset oracle) 1
@@ -166,12 +177,13 @@ updateOracle oracle x = do
             let lookups = Constraints.unspentOutputs (Map.singleton oref o)     <>
                           Constraints.typedValidatorLookups (oracleInst oracle) <>
                           Constraints.otherScript (oracleValidator oracle)
-                tx      = c <> Constraints.mustSpendScriptOutput oref (Redeemer $ PlutusTx.toData Update)
+                tx      = c <> Constraints.mustSpendScriptOutput oref (Redeemer $ PlutusTx.toBuiltinData Update)
             ledgerTx <- submitTxConstraintsWith @Oracling lookups tx
             awaitTxConfirmed $ txId ledgerTx
             logInfo @Prelude.String $ "updated oracle value to " ++ Prelude.show x
 
-findOracle :: forall w s. HasBlockchainActions s => Oracle -> Contract w s Text (Maybe (TxOutRef, TxOutTx, Integer))
+-- Helper function to find oracle from list of utxos that are present at oracle address
+findOracle :: forall w s. Oracle -> Contract w s Text (Maybe (TxOutRef, TxOutTx, Integer))
 findOracle oracle = do
     utxos <- Map.filter f <$> utxoAt (oracleAddress oracle)
     return $ case Map.toList utxos of
@@ -183,26 +195,34 @@ findOracle oracle = do
     f :: TxOutTx -> Bool
     f o = assetClassValueOf (txOutValue $ txOutTxOut o) (oracleNftAsset oracle) == 1
 
-type OracleSchema = BlockchainActions .\/ Endpoint "update" Integer
+type OracleSchema = Endpoint "update" Integer
 
+errorHandler :: Prelude.Show a => a -> Contract w s e ()
+errorHandler e = do
+    Contract.logError $ Prelude.show e
+
+oracleContract :: Contract (Last Oracle) OracleSchema Text ()
+oracleContract = handleError errorHandler runOracle
+
+--Contract for running oracle contract by first constructing a oracle paramter
 runOracle :: Contract (Last Oracle) OracleSchema Text ()
 runOracle = do
     oracle <- startOracle
     tell $ Last $ Just oracle
-    go oracle
+    awaitPromise $ go oracle
   where
-    go :: Oracle -> Contract (Last Oracle) OracleSchema Text a
-    go oracle = do
-        x <- endpoint @"update"
-        updateOracle oracle x
-        go oracle
+    go :: Oracle -> Promise (Last Oracle) OracleSchema Text ()
+    go oracle' = 
+        (endpoint @"update") $ \x -> do
+          updateOracle oracle' x
+          awaitPromise $ go oracle'
 
+--Run mock oracle to be used in test with default oracle provided form outside of contract
 runMockOracle :: Oracle -> Contract () OracleSchema Text ()
-runMockOracle oracle = do
-    go oracle
+runMockOracle oracle = 
+    handleError errorHandler $ awaitPromise (go oracle) >> runMockOracle oracle
   where
-    go :: Oracle -> Contract () OracleSchema Text a
-    go oracle' = do
-        x <- endpoint @"update"
-        updateOracle oracle' x
-        go oracle'
+    go :: Oracle -> Promise () OracleSchema Text ()
+    go oracle' = 
+        (endpoint @"update") $ \x -> do
+          updateOracle oracle' x
