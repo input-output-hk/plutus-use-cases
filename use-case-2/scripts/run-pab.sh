728ad1d090f012d555c5b89205ec4fa2cc62dfac
Ryan Trinkle <ryan.trinkle@gmail.com>
2021-09-24 12:53:51 -0400

    Check port 8080 before running PAB

--- use-case-2/scripts/run-pab.sh
+++ use-case-2/scripts/run-pab.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
+if netstat -nlt | awk '{print $4}' | grep -q ':8080' ; then
+    echo "Port 8080 is in use; please stop any process using port 8080 and then try again"
+    exit 1
+fi
+
 "$(nix-build "$DIR"/.. -A plutus-starter.haskellNixProject.hsPkgs.plutus-starter.components.exes.plutus-starter-pab --no-out-link)"/bin/plutus-starter-pab
