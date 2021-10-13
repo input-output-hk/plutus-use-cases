c5cb0d5e54e4100310824f4815ceddb0616291cd
olgaklimenko <gumper190@gmail.com>
2021-10-13 13:58:47 +0700

    add todo about DiffMilliSeconds

--- MetaLamp/nft-marketplace/src/Plutus/Contracts/NftMarketplace/OffChain/User.hs
+++ MetaLamp/nft-marketplace/src/Plutus/Contracts/NftMarketplace/OffChain/User.hs
@@ -186,12 +186,10 @@ closeSale marketplace CloseLotParams {..} = do
     logInfo @Haskell.String $ printf "Closed lot sale %s" (Haskell.show sale)
     pure ()
 
--- deriving newtype instance Schema.ToSchema DiffMilliSeconds
-
 data StartAnAuctionParams =
   StartAnAuctionParams {
     saapItemId   :: UserItemId,
-    saapDuration :: Integer
+    saapDuration :: Integer  --- TODO: use DiffMilliSeconds here, when it will be possible 
   }
     deriving stock    (Haskell.Eq, Haskell.Show, Haskell.Generic)
     deriving anyclass (J.ToJSON, J.FromJSON, Schema.ToSchema)
