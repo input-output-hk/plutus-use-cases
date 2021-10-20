3f7ab3e40017fd91e110042a4e0596d606b0d047
Mariana.Berezina <mariana.mysak@eleks.com>
2021-10-20 11:54:59 +0300

    Enhancements, goals and rewards

--- eleks/oracle/games-web-app/src/components/Game.js
+++ eleks/oracle/games-web-app/src/components/Game.js
@@ -15,17 +15,19 @@ import {
   getGameBetsFetching,
 } from '../reducers';
 import { statusesMap } from '../helpers/constants';
+import { lovelaceToAda } from '../helpers/utils';
 
 import '../styles/Game.scss';
 
 const Game = ({
   game,
+  myReward,
   homeBets,
   awayBets,
   showModal,
   setShowModal,
-  homeBorder,
-  awayBorder,
+  homeColor,
+  awayColor,
   currentUser,
   gameFetching,
   betsFetching,
@@ -35,7 +37,7 @@ const Game = ({
       {game && game.fixture && (
         <>
           <section className='team'>
-            <div className={`img-border ${homeBorder}`}>
+            <div className={`img-border ${homeColor}`}>
               <img
                 src={game.teams.home.logo}
                 alt='team-logo'
@@ -56,7 +58,27 @@ const Game = ({
             <span className='date'>
               {moment(game.fixture.date).format('DD MMM HH:mm YYYY')}
             </span>
-            <span className='referee'>Referee: {game.fixture.referee}</span>
+            <span className='details'>
+              <b>Referee:</b> {game.fixture.referee}
+            </span>
+            <span className='details'>
+              <b>City:</b> {game.fixture.venue.city}
+            </span>
+            <span className='details place'>
+              <b>Place:</b> {game.fixture.venue.name}
+            </span>
+            {(game.fixture.status.short === 'FT' ||
+              game.fixture.status.short === 'LIVE') && (
+              <div className='goals-container'>
+                <span className={`goals ${homeColor}`}>{game.goals.home}</span>{' '}
+                -<span className={`goals ${awayColor}`}>{game.goals.away}</span>
+              </div>
+            )}
+            {game.fixture.status.short === 'FT' && myReward !== 0 && (
+              <span className='reward'>
+                You won <b>{lovelaceToAda(myReward)}</b> ADA
+              </span>
+            )}
             {game.fixture.status.short === 'NS' && (
               <Button variant='secondary' onClick={() => setShowModal(true)}>
                 Make a bet
@@ -64,7 +86,7 @@ const Game = ({
             )}
           </section>
           <section className='team'>
-            <div className={`img-border ${awayBorder}`}>
+            <div className={`img-border ${awayColor}`}>
               <img
                 src={game.teams.away.logo}
                 alt='team-logo'
@@ -118,26 +140,37 @@ const enhancer = compose(
       fetchGameBets: (id) => dispatch(fetchGameBets(id)),
     })
   ),
-  withProps(({ bets, game }) => ({
-    homeBets: bets
-      ? bets.filter((bet) => bet.betTeamId === game.teams.home.id)
-      : [],
-    awayBets: bets
-      ? bets.filter((bet) => bet.betTeamId === game.teams.away.id)
-      : [],
-    homeBorder:
-      game && game.fixture && game.fixture.status.short === 'FT'
-        ? game.teams.home.winner
-          ? 'winner'
-          : 'defeated'
-        : '',
-    awayBorder:
-      game && game.fixture && game.fixture.status.short === 'FT'
-        ? game.teams.away.winner
-          ? 'winner'
-          : 'defeated'
-        : '',
-  })),
+  withProps(({ bets, game, currentUser }) => {
+    const myBets = bets
+      ? bets.filter(
+          (bet) => bet.betBettor.getPubKeyHash === currentUser.publicKey
+        )
+      : [];
+    return {
+      myReward:
+      game && game.fixture.status.short === 'FT'
+          ? myBets.reduce((acc, curr) => acc + curr.winShare.getLovelace, 0)
+          : 0,
+      homeBets: bets
+        ? bets.filter((bet) => bet.betTeamId === game.teams.home.id)
+        : [],
+      awayBets: bets
+        ? bets.filter((bet) => bet.betTeamId === game.teams.away.id)
+        : [],
+      homeColor:
+        game && game.fixture && game.fixture.status.short === 'FT'
+          ? game.teams.home.winner
+            ? 'winner'
+            : 'defeated'
+          : '',
+      awayColor:
+        game && game.fixture && game.fixture.status.short === 'FT'
+          ? game.teams.away.winner
+            ? 'winner'
+            : 'defeated'
+          : '',
+    };
+  }),
   lifecycle({
     componentDidMount() {
       const gameId = this.props.match.params.id;
--- eleks/oracle/games-web-app/src/reducers/bets.js
+++ eleks/oracle/games-web-app/src/reducers/bets.js
@@ -3,7 +3,9 @@ import {
   FETCH_GAME_BETS_START,
   FETCH_GAME_BETS_SUCCESS,
   FETCH_GAME_BETS_FAILED,
+  FETCH_MAKE_BET_START,
   FETCH_MAKE_BET_SUCCESS,
+  FETCH_MAKE_BET_FAILED,
   LOGOUT,
 } from '../helpers/actionTypes';
 
@@ -12,7 +14,7 @@ export const data = (state = null, action) => {
     case FETCH_GAME_BETS_SUCCESS:
       return action.bets;
     case FETCH_MAKE_BET_SUCCESS:
-      return state ? [...state, action.bet] : [action.bet];
+      return state ? [action.bet, ...state] : [action.bet];
     case LOGOUT:
       return null;
     default:
@@ -23,9 +25,12 @@ export const data = (state = null, action) => {
 export const fetching = (state = false, action) => {
   switch (action.type) {
     case FETCH_GAME_BETS_START:
+    case FETCH_MAKE_BET_START:
       return true;
     case FETCH_GAME_BETS_SUCCESS:
     case FETCH_GAME_BETS_FAILED:
+    case FETCH_MAKE_BET_SUCCESS:
+    case FETCH_MAKE_BET_FAILED:
       return false;
     default:
       return state;
@@ -35,8 +40,10 @@ export const fetching = (state = false, action) => {
 export const error = (state = '', action) => {
   switch (action.type) {
     case FETCH_GAME_BETS_SUCCESS:
+    case FETCH_MAKE_BET_SUCCESS:
       return '';
     case FETCH_GAME_BETS_FAILED:
+    case FETCH_MAKE_BET_FAILED:
       return action.error;
     default:
       return state;
