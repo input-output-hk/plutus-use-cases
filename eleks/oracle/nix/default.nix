6a7e68ae59d251f2c2b7b000e95d76133ae96b65
volodymyr derecha <volodymyr.derecha@eleks.com>
2021-11-08 11:26:49 +0200

    update nix version

--- eleks/oracle/nix/default.nix
+++ eleks/oracle/nix/default.nix
@@ -5,12 +5,12 @@ let
   # We're going to get everything from the main plutus repository. This ensures
   # we're using the same version of multiple dependencies such as nipxkgs,
   # haskell-nix, cabal-install, compiler-nix-name, etc.
-  plutus = import sources.plutus {};
+  plutus = import sources.plutus-apps {};
   pkgs = plutus.pkgs;
 
   haskell-nix = pkgs.haskell-nix;
 
- plutus-starter = import ./pkgs {
+  plutus-starter = import ./pkgs {
     inherit pkgs haskell-nix sources plutus;
   };
 
