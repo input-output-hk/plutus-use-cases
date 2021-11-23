98ed5b807c1e27c93a0c052cc2f69c66bb7c91dc
olgaklimenko <gumper190@gmail.com>
2021-11-23 10:24:00 +0700

    update to v2021-11-22

--- MetaLamp/nft-marketplace/nix/pkgs/haskell/haskell.nix
+++ MetaLamp/nft-marketplace/nix/pkgs/haskell/haskell.nix
@@ -19,7 +19,7 @@ let
     inherit compiler-nix-name;
 
     sha256map = {
-      "https://github.com/input-output-hk/plutus-apps.git"."7333e3c3ae62c955c5db7799dbf1c1f58593325d" = "1j5wwi9q71gbsakcvq67phpi4nha643aw711c0zyfjs5sd2r5hzb";
+      "https://github.com/input-output-hk/plutus-apps.git"."v2021-11-22" = "10m0gmnakjnpnzyvjs3ksfpxgjcq3pdphr4gf8v7fjhr9fjbc45n";
       "https://github.com/Quid2/flat.git"."ee59880f47ab835dbd73bea0847dab7869fc20d8" = "1lrzknw765pz2j97nvv9ip3l1mcpf2zr4n56hwlz0rk7wq7ls4cm";
       "https://github.com/input-output-hk/purescript-bridge.git"."366fc70b341e2633f3ad0158a577d52e1cd2b138" = "18j0rysfccbmfpbw2d1rsjkpd5h84alpsn6b5rwzdxw9h5vqi9m5";
       "https://github.com/input-output-hk/servant-purescript.git"."ebea59c7bdfc0338d83fca772b9a57e28560bcde" = "0gjcq4y61kwb4w70pnswn5dp23wd13dac8d9hz84j374cm1kshsn";
