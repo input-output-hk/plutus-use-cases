3e9278eee4f0a21a659a02148cc03c2e5073fbbb
volodymyr derecha <volodymyr.derecha@eleks.com>
2021-12-06 23:04:07 +0200

    update to version

--- eleks/oracle/src/Contracts/Oracle/OffChain.hs
+++ eleks/oracle/src/Contracts/Oracle/OffChain.hs
@@ -13,6 +13,7 @@
 {-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE NumericUnderscores    #-}
 {-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE LambdaCase            #-}
 
 module Contracts.Oracle.OffChain
     ( OracleSchema
@@ -51,6 +52,7 @@ import           Data.Maybe                (catMaybes)
 import           Data.Monoid               (Last (..))
 import           Data.Text                 (Text, pack, unpack)
 import qualified Data.List.NonEmpty        as NonEmpty
+import           Data.Void (Void)
 import           GHC.Generics              (Generic)
 import           Plutus.Contract           as Contract
 import qualified PlutusTx
@@ -61,7 +63,7 @@ import qualified Ledger.Scripts            as LedgerScripts
 import qualified Ledger.Tx                 as LedgerScripts
 import           Ledger.Constraints        as Constraints
 import qualified Ledger.Contexts           as Validation
-import           Ledger.Oracle             (Observation, SignedMessage(..), signMessage, SignedMessageCheckError(..), checkSignature)
+import           Plutus.Contract.Oracle    (Observation, SignedMessage(..), signMessage, SignedMessageCheckError(..), checkSignature)
 import qualified Ledger.Typed.Scripts      as Scripts
 import           Ledger.Value              as Value
 import           Ledger.Ada                as Ada
@@ -122,10 +124,26 @@ updateOracle oracle operatorPrivateKey params = do
                             logInfo ("submit transaction " ++ (show $ oracleData'))
                             mkTxConstraints lookups tx >>= submitTxConfirmed . adjustUnbalancedTx
 
-oracleValueFromTxOutTx :: ChainIndexTxOut -> Maybe OracleData
-oracleValueFromTxOutTx o = do
-    Datum d <- either (const Nothing) Just (_ciTxOutDatum o)
-    PlutusTx.fromBuiltinData d
+-- oracleValueFromTxOutTx :: ChainIndexTxOut -> Maybe OracleData
+-- oracleValueFromTxOutTx o = do
+--     Datum d <- either (const Nothing) Just (_ciTxOutDatum o)
+--     PlutusTx.fromBuiltinData d
+
+oracleValueFromTxOutTx :: ChainIndexTxOut -> Contract w s Text OracleData
+oracleValueFromTxOutTx o = 
+    case o of
+      PublicKeyChainIndexTxOut {} ->
+        throwError "no datum for a txout of a public key address"
+      ScriptChainIndexTxOut { _ciTxOutDatum } -> do
+        (Datum e) <- either getDatum pure _ciTxOutDatum
+        maybe (throwError "datum hash wrong type")
+            pure
+            (PlutusTx.fromBuiltinData e)
+  where
+    getDatum :: DatumHash -> Contract w s Text Datum
+    getDatum dh =
+      datumFromHash dh >>= \case Nothing -> throwError "datum not found"
+                                 Just d  -> pure d
 
 data UpdateOracleParams = UpdateOracleParams
     { uoGameId      :: !GameId              -- ^ Game
@@ -147,7 +165,9 @@ requestOracleForAddress oracle gameId = do
     logInfo @Haskell.String "Request oracle for address"
     pkh <- Contract.ownPubKeyHash
     let inst = typedOracleValidator oracle
+        operatorKey = (oOperator oracle)
         mrScript = oracleValidator oracle
+        address = oracleAddress oracle
         tokenMintingPolicy = requestTokenPolicy $ oracleToRequestToken oracle
         forgedToken = requestTokenValue oracle
         oracleFee = oFee oracle 
@@ -159,7 +179,10 @@ requestOracleForAddress oracle gameId = do
             , ovSignedMessage = Nothing
             }
         mintRedeemer = Redeemer $ PlutusTx.toBuiltinData $ Request
-    let lookups = Constraints.typedValidatorLookups inst 
+    logInfo @Haskell.String $ "Oracle address: " ++ show address
+    logInfo @Haskell.String $ "Oracle data: " ++ show oracleData
+    let lookups = 
+                Constraints.typedValidatorLookups inst 
                 <> Constraints.otherScript mrScript
                 <> Constraints.mintingPolicy tokenMintingPolicy
 
@@ -167,28 +190,27 @@ requestOracleForAddress oracle gameId = do
                 <> Constraints.mustPayToPubKey (oOperator oracle) (feeVal)
                 <> Constraints.mustMintValueWithRedeemer mintRedeemer forgedToken
 
-    --mkTxConstraints lookups tx >>= submitTxConfirmed . adjustUnbalancedTx
-    --logInfo @Haskell.String "Requested oracle for address"
-    --result <-  either (pure . Left) (runError $  submitTxConstraintsWith @Oracling lookups tx)
-
-    --handleError (\err -> logInfo $ "caught error: " ++ unpack err) $ void $ mkTxConstraints lookups tx >>= submitTxConfirmed . adjustUnbalancedTx
-    result <- runError @_ @_ @Text $ submitTxConstraintsWith @Oracling lookups tx
-    case result of
-        Left err -> do
-            logWarn @Haskell.String "An error occurred. Request oracle for address failed."
-            logWarn err
-        Right tx -> do
-            let txi = getCardanoTxId tx
-            logInfo @Haskell.String $ "Waiting for tx " <> Haskell.show txi <> " to complete"
-            awaitTxConfirmed txi
-            logInfo @Haskell.String "Tx confirmed. Request oracle for address complete."
+    handleError (\err -> logInfo $ "caught error: " ++ unpack err) $ mkTxConstraints @Oracling lookups tx >>= submitTxConfirmed . adjustUnbalancedTx
+    logInfo @Haskell.String "Request oracle for address complete."
+    -- result <- runError @_ @_ @Text $ submitTxConstraintsWith @Oracling lookups tx
+    -- case result of
+    --     Left err -> do
+    --         logWarn @Haskell.String "An error occurred. Request oracle for address failed."
+    --         logWarn err
+    --     Right tx -> do
+    --         let txi = getCardanoTxId tx
+    --         logInfo @Haskell.String $ "Waiting for tx " <> Haskell.show txi <> " to complete"
+    --         awaitTxConfirmed txi
+    --         logInfo @Haskell.String "Tx confirmed. Request oracle for address complete."
 
 --get active request lists for oracle to process
 getActiveOracleRequests:: Oracle -> Contract w s Text [(TxOutRef, ChainIndexTxOut, OracleData)]
 getActiveOracleRequests oracle = do
-    xs <- utxosAt (oracleAddress oracle)
+    let address = oracleAddress oracle
+    logInfo @Haskell.String $ "Oracle address: " ++ show address
+    xs <- utxosAt address
     logInfo @Haskell.String ("Get active utxos: " ++ show xs)
-    let filtered = filterOracleRequest oracle . Map.toList $ xs
+    filtered <- Control.Monad.mapM mapDatum . filterOracleRequest oracle . Map.toList $ xs
     logInfo @Haskell.String ("Filtered: " ++ show filtered)
     let requests = filter (isActiveRequest oracle) . rights $ filtered
     return requests 
@@ -209,8 +231,9 @@ awaitNextOracleRequest oracle =
         utxos <- awaitUtxoProduced $ oracleAddress oracle
         let txs = map (\tx -> map convertChainIndexOut $ chainIndexTxOutsWithRef tx) (NonEmpty.toList $ utxos)
         let filterValidTx = catMaybes . concat
-        let filtered = rights . filterOracleRequest oracle . filterValidTx $ txs
-        return filtered
+        oracleRequests <- Control.Monad.mapM mapDatum . filterOracleRequest oracle . filterValidTx $ txs
+        let requestWithDatum = rights oracleRequests
+        return requestWithDatum
             
 runOracle :: OracleParams -> Contract (Last OracleContractState) OracleSchema Text ()
 runOracle op = do
@@ -240,16 +263,20 @@ hasOracleRequestToken :: Oracle -> (TxOutRef, ChainIndexTxOut) -> Bool
 hasOracleRequestToken oracle (oref, o) = 
     assetClassValueOf (view ciTxOutValue o) (requestTokenClassFromOracle oracle) == 1
 
-hasOracelRequestDatum :: (TxOutRef, ChainIndexTxOut) -> Bool
-hasOracelRequestDatum (oref, o) = isJust . oracleValueFromTxOutTx $ o
+filterOracleRequest :: Oracle -> [(TxOutRef, ChainIndexTxOut)] -> [(TxOutRef, ChainIndexTxOut)]
+filterOracleRequest oracle txs = filter (hasOracleRequestToken oracle) $ txs
 
-filterOracleRequest :: Oracle -> [(TxOutRef, ChainIndexTxOut)] -> [Either Text (TxOutRef, ChainIndexTxOut, OracleData)]
-filterOracleRequest oracle txs = map mapDatum . filter (hasOracleRequestToken oracle) $ txs
+-- mapDatum :: (TxOutRef, ChainIndexTxOut) -> Either Text (TxOutRef, ChainIndexTxOut, OracleData)
+-- mapDatum (oref, o) = case oracleValueFromTxOutTx o of
+--     Just datum -> Right (oref, o, datum)
+--     Nothing -> Left "No datum"
 
-mapDatum :: (TxOutRef, ChainIndexTxOut) -> Either Text (TxOutRef, ChainIndexTxOut, OracleData)
-mapDatum (oref, o) = case oracleValueFromTxOutTx o of
-    Just datum -> Right (oref, o, datum)
-    Nothing -> Left "No datum"
+mapDatum :: forall w s. (TxOutRef, ChainIndexTxOut) -> Contract w s Text (Either Text (TxOutRef, ChainIndexTxOut, OracleData))
+mapDatum (oref, o) = do
+    datumE <- runError @_ @_ @Text $ oracleValueFromTxOutTx o
+    case datumE of
+        Right datum -> pure $ Right (oref, o, datum)
+        Left err -> pure $ Left err
 
 isGameOracleRequest :: GameId -> (TxOutRef, ChainIndexTxOut, OracleData) -> Bool
 isGameOracleRequest gameId (_, _, od) = gameId == (ovGame od) 
@@ -277,14 +304,9 @@ findOracleRequest ::
 findOracleRequest oracle gameId owner = do
     xs <- utxosAt (oracleAddress oracle)
     let findCriteria = find (\tx -> isOwnerOracleRequest owner tx && isGameOracleRequest gameId tx)
-    let request = findCriteria . rights .filterOracleRequest oracle . Map.toList $ xs
+    requestsWithDatum <- Control.Monad.mapM mapDatum . filterOracleRequest oracle . Map.toList $ xs
+    let request = findCriteria . rights $ requestsWithDatum
     pure request 
-
-data UseOracleParams = UseOracleParams
-    { uoGame           :: Integer -- use owned oracle request
-    }
-    deriving stock (Haskell.Eq, Haskell.Show, Generic)
-    deriving anyclass (ToJSON, FromJSON, ToSchema)
     
 type UseOracleSchema = Endpoint "use" UseOracleParams
 
