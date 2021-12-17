8e7816ddf6a8c3776d87bf9c462c9862bb9f6f33
olgaklimenko <oklimenko92@mail.ru>
2021-12-17 16:36:14 +0700

    update to node-1.32.1 walllet-2021.12.15

--- MetaLamp/nft-marketplace/deployment/env/env.template.sh
+++ MetaLamp/nft-marketplace/deployment/env/env.template.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export NODE_PATH="/home/cardano/testnet"  # set a path to testnet configs and db
-export NODE_TAG="1.30.1"                  # cardano-node tag
+export NODE_TAG="1.32.1"                  # cardano-node tag
