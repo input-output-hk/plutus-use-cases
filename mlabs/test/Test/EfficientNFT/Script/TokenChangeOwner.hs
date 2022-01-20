bcb5045660305e73d4545ead4ac040291deca812
gege251 <gege251@gmail.com>
2022-01-20 14:12:20 +0100

    Add a property test for ChangeOwner action

--- mlabs/test/Test/EfficientNFT/Script/TokenChangeOwner.hs
+++ mlabs/test/Test/EfficientNFT/Script/TokenChangeOwner.hs
@@ -1,14 +1,10 @@
 module Test.EfficientNFT.Script.TokenChangeOwner (test) where
 
-import Ledger (
-  MintingPolicy,
-  PaymentPubKeyHash (unPaymentPubKeyHash),
-  mkMintingPolicyScript,
- )
+import Ledger (PaymentPubKeyHash (unPaymentPubKeyHash))
 import Ledger.Value (CurrencySymbol, TokenName (TokenName, unTokenName))
 import Ledger.Value qualified as Value
 import PlutusTx qualified
-import PlutusTx.Positive (positive)
+import PlutusTx.Positive (Positive, positive)
 
 import Data.Data (Typeable)
 import Data.List.NonEmpty (NonEmpty)
@@ -26,8 +22,10 @@ import Test.Tasty.Plutus.Context (
   paysToPubKeyWithDatum,
  )
 import Test.Tasty.Plutus.Options (TestCurrencySymbol (TestCurrencySymbol))
+import Test.Tasty.Plutus.Script.Property (scriptPropertyFail)
 import Test.Tasty.Plutus.Script.Unit (shouldValidate, shouldn'tValidateTracing)
 import Test.Tasty.Plutus.TestData (
+  Generator (GenForMinting),
   Methodology,
   MintingPolicyTask,
   Outcome,
@@ -65,6 +63,15 @@ test =
           oldTokenNotBurntData
           validCtx
 
+        shouldFailWithErr
+          "Fail if new token is not minted"
+          "Old version must be burnt when reminting"
+          newTokenNotMintedData
+          validCtx
+
+        scriptPropertyFail "Should mint and burn exactly 1 token" $
+          GenForMinting fromArbitrary oneTokenMintAndBurn
+
         shouldValidate
           "Pass if additional tokens (non-NFT) minted"
           validData
@@ -95,6 +102,12 @@ oldTokenNotBurntData :: TestData ( 'ForMinting MintAct)
 oldTokenNotBurntData = MintingTest redeemer tasks
   where
     tasks = mintTokens (Tokens validNewTokenName [positive| 1 |])
+    redeemer = ChangeOwner TestValues.nft2 TestValues.userTwoPkh
+
+newTokenNotMintedData :: TestData ( 'ForMinting MintAct)
+newTokenNotMintedData = MintingTest redeemer tasks
+  where
+    tasks = burnTokens (Tokens validOldTokenName [positive| 1 |])
     redeemer = ChangeOwner TestValues.nft2 TestValues.userTwoPkh
 
 validOldTokenName :: TokenName
@@ -141,6 +154,30 @@ manyTokensCtx =
     <> mintsValue additionalValue
   where
     additionalValue = Value.singleton (Value.CurrencySymbol "aa") (TokenName "ff") 1
+
+-- | Creates TestItems with an arbitrary key used in Redeemer
+oneTokenMintAndBurn :: (Positive, Positive) -> TestItems ( 'ForMinting MintAct)
+oneTokenMintAndBurn (mintAmt, burnAmt) =
+  ItemsForMinting
+    { mpRedeemer = redeemer
+    , mpTasks = tasks
+    , mpCB = validCtx
+    , mpOutcome = out
+    }
+  where
+    mintAmt' = mintAmt + [positive| 1 |]
+    burnAmt' = mintAmt + [positive| 1 |]
+
+    toksMint = Tokens validNewTokenName mintAmt'
+    toksBurn = Tokens validOldTokenName burnAmt'
+
+    redeemer = ChangeOwner TestValues.nft2 TestValues.userTwoPkh
+
+    tasks :: NonEmpty MintingPolicyTask
+    tasks = mintTokens toksMint <> burnTokens toksBurn
+
+    out :: Outcome
+    out = passIf $ fromEnum mintAmt' == 1 && fromEnum burnAmt' == 1
 
 testTokenCurSym :: CurrencySymbol
 testTokenCurSym = "aabbcc"
